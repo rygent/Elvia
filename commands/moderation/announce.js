784d1337945bcd735cd63f26cbac94d6bb49243f
RzkyX <marizky65@gmail.com>
2019-12-02 03:40:41 +0700

    Added announcement command

---
+++ commands/moderation/announce.js
@@ -0,0 +1,26 @@
+const { RichEmbed } = require('discord.js')
+
+module.exports = {
+    config: {
+        name: 'announce',
+        aliases: ['bc', 'broadcast'],
+        category: 'moderation',
+        description: 'Send an announcement using the bot.',
+        usage: '<input>',
+        accessableby: 'Moderators'
+    },
+    run: async (bot, message, args) => {
+        message.delete()
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000))
+
+        const roleColor = message.guild.me.highestRole.hexColor
+
+        const embed = new RichEmbed()
+        .setDescription(args.join(" "))
+        .setColor(roleColor === "#000000" ? "#ffffff" : roleColor)
+
+        message.channel.send(embed)
+    }
+}
