f79dc012379405ccfd5bb11371ca30479f0e2ca9
RzkyX <marizky65@gmail.com>
2019-12-02 03:39:32 +0700

    Added new status

--- events/client/ready.js
+++ events/client/ready.js
@@ -9,7 +9,10 @@ module.exports = bot => {
         `Enjoying my time with you!`, `Can i get some more of that?`,
         `Don't you all love me? ...Oh well.`, `Doing bot things.`,
         `What about we go out and have some cake?`, `Now with more splashes!`,
-        `Wait, do I even have this?`, `I am also a CPU. No, really!`
+        `Wait, do I even have this?`, `I am also a CPU. No, really!`,
+        `That's fine, 'cause nothing's gonna scare us now!`, `A little bit of this... a little bit of that... yes, that's it!`,
+        `Having fun with ya!`, `Maximum explosion for maximum justice!`,
+        `Now singing along you.`, `Damn! I should've finished my backlog before formatting Windows.`
     ], i = 0;
 
     setInterval(() => bot.user.setActivity(`${bot.prefix}help | ${activities[i++ % activities.length]} | [${bot.users.size}]`, 
