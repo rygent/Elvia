598566aa02dd734324d41ecf3f596c97dc9d50ae
RzkyX <marizky65@gmail.com>
2019-12-02 03:50:34 +0700

    Remove chat from console

--- handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-    let prompt = process.openStdin()
-    prompt.addListener('data', res => {
-        let x = res.toString().trim().split(/ +/g)
-        bot.channels.get('646882466333851672').send(x.join(' '))
-    })
-}
--- index.js
+++ index.js
@@ -8,6 +8,6 @@ config({
 
 bot.prefix = '!';
 ['aliases', 'commands'].forEach(x => bot[x] = new Collection());
-['console', 'command', 'event'].forEach(x => require(`./handlers/${x}`)(bot));
+['command', 'event'].forEach(x => require(`./handlers/${x}`)(bot));
 
 bot.login(process.env.TOKEN);
