698ab5a015b2f69e4ffadf06d827c2c51527f6e4
RzkyX <marizky65@gmail.com>
2020-01-19 06:48:25 +0700

    Create djs.js

---
+++ commands/utility/djs.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+    config: {
+        name: 'djs',
+        aliases: ['discord-js'],
+        category: 'utility',
+        description: 'Searches the DJS docs for whatever you\'d like',
+        usage: '<query> (branch)',
+        accessableby: 'Members'
+    },
+    run: async (bot, message, args) => {
+        const query = args.join(' ');
+        const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
+        
+        fetch(url)
+        .then(res => res.json())
+        .then(embed => {
+            if(embed && !embed.error) {
+                message.channel.send({ embed });
+            } else {
+                message.reply(`I don't know mate, but "${query}" doesn't make any sense!`);
+            }
+        })
+        .catch(e => {
+            console.error(e);
+            message.reply('Darn it! I failed!');
+        })
+    }
+};
