fd59be29f7cc79abb3062698869885637328ed80
RzkyX <marizky65@gmail.com>
2020-01-22 05:13:18 +0700

    Added setnick.js

---
+++ commands/moderation/setnick.js
@@ -0,0 +1,41 @@
+const { RichEmbed } = require('discord.js');
+const { Colors } = require('../../settings');
+
+module.exports = {
+    config: {
+        name: 'setnick',
+        aliases: ['nick'],
+        category: 'moderation',
+        description: 'Set a user or bot\'s nickname',
+        usage: '<@user> <nickname>',
+        accessableby: 'Moderations'
+    },
+    run: async (bot, message, args) => {
+        if(!message.member.hasPermission(['MANAGE_NICKNAMES'])) 
+            return message.channel.send('⛔ *You cannot execute that command due to invaild permissions.*');
+        
+        let nUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!nUser) return message.channel.send(`**${message.author.username}**, Please specify a user that is on this server.`);
+        
+        let nick = args.join(" ").slice(22);
+        if(!nick) return message.reply("Please specify a name.");
+        
+        if(nUser.hasPermission('MANAGE_NICKNAMES')) return message.channel.send("That person can't have their name changed.");
+            
+        message.delete();
+        
+        await(nUser.setNickname(nick));
+
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        let nembed = new RichEmbed()
+            .setColor(roleColor === '#000000' ? '#ffffff' : roleColor)
+            .setAuthor(nUser.user.tag, nUser.user.displayAvatarURL)
+            .setDescription(`Successfully change nickname to ${nick}`)
+            .setFooter(`ID: ${nUser.user.id}`)
+            .setTimestamp();
+        
+        let sChannel = message.guild.channels.find(c => c.name === "incident-log")
+        sChannel.send(nembed);
+    }    
+}
