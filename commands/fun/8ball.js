4e6e1d1f423b110fbf31e75787ec811472644ae9
RzkyX <marizky65@gmail.com>
2020-01-23 08:12:56 +0700

    Remove assets script

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,4 +1,5 @@
 const { RichEmbed } = require('discord.js');
+const replies = require('../../assets/json/8ball');
 
 module.exports = {
     config: {
@@ -11,54 +12,6 @@ module.exports = {
     },
     run: async (bot, message, args) => {
         if (!args[1]) return message.reply('Plase ask a full question!');
-        let replies = [
-            'It is certain.',
-            'It is decidedly so.',
-            'Without a doubt.',
-            'Yes - definitely.',
-            'You may rely on it.',
-            'As I see it, yes.',
-            'Most likely.',
-            'Outlook good.',
-            'Yes.',
-            'Signs point to yes.',
-            'Reply hazy, try again.',
-            'Ask again later.',
-            'Better not tell you now.',
-            'Cannot predict now.',
-            'Concentrate and ask again.',
-            'Dont count on it.',
-            'My reply is no.',
-            'My sources say no.',
-            'Outlook not so good.',
-            'Very doubtful.',
-            'Maybe',
-            'That is sure as hell.',
-            'Fire.',
-            'Indeed.',
-            'Try to be usefull.',
-            'Watch the birds.',
-            'Gold.',
-            'Answer is uncertain.',
-            'You are the master of your life',
-            'Maybe no.',
-            'We can not be never sure.',
-            'As you wish.',
-            'Eat less, move more.',
-            'Better ask yourself.',
-            'Just do it.',
-            'Sorry, but this is really stupid question.',
-            'Try to be usefull.',
-            'Water.',
-            'We can not be never sure.',
-            'You already know the Answer.',
-            'Very bad idea.',
-            'Never.',
-            'Maybe yes.',
-            'Maybe no.',
-            'NO',
-            'YES'
-        ];
         
         let result = Math.floor(Math.random() * replies.length);
         let question = args.slice(0).join(' ');
