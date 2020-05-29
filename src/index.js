38bcc9cf9a3b319d52334e439c073a4e33944603
XRzky <marizky65@gmail.com>
2020-05-29 09:30:28 +0700

    Initial Commit
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-process-env */
+require('dotenv').config();
+
+const RivenClient = require('./structures/RivenClient.js');
+const config = process.env;
+
+const client = new RivenClient(config);
+
+client.login();
---
+++ src/structures/RivenClient.js
@@ -0,0 +1,51 @@
+const { Client } = require('discord.js');
+
+module.exports = class RivenClient extends Client {
+
+	constructor(options = {}) {
+		super({
+			disableMentions: 'everyone'
+		});
+		this.validate(options);
+
+		this.once('ready', () => {
+			console.log(`Logged in as ${this.user.username}!`);
+		});
+
+		this.on('message', async (message) => {
+			const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
+			const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
+
+			if (!message.guild || message.author.bot) return;
+
+			if (message.content.match(mentionRegex)) {
+				message.channel.send(`My prefix for ${message.guild.name} is \`${this.PREFIX}\`.`);
+			}
+
+			const prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : this.PREFIX;
+
+			// eslint-disable-next-line no-unused-vars
+			const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+
+			if (cmd.toLowerCase() === 'hello') {
+				message.channel.send('Hello!');
+			}
+		});
+	}
+
+	validate(options) {
+		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
+
+		if (!options.token) throw new Error('You must pass the token for the client.');
+		this.TOKEN = options.token;
+
+		if (!options.prefix) throw new Error('You must pass a prefix for the client.');
+		if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
+		this.PREFIX = options.prefix;
+	}
+
+	async login(token = this.TOKEN) {
+		super.login(token);
+	}
+
+};
