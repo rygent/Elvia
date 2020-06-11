5e201a5c6b2f96b6868ae09bd64da5addd84e36c
XRzky <marizky65@gmail.com>
2020-06-11 19:45:17 +0700

    Commands: Added invite
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/information/invite.js
@@ -0,0 +1,33 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+const { stripIndents } = require('common-tags');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'invite',
+			aliases: [],
+			description: 'Gives you the invite link!',
+			category: 'information'
+		});
+	}
+
+	async run(message) {
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		const embed = new MessageEmbed()
+			.setColor(roleColor === '#000000' ? Colors.CUSTOM : roleColor)
+			.setTitle('__Invitation Link__')
+			.setDescription(stripIndents`
+                Here is the bot invite link and support server invite link!
+                [Invite ${this.client.user.username}](https://discordapp.com/oauth2/authorize?&client_id=${this.client.user.id}&scope=bot&permissions=1043721303)
+                [Support Server](https://discord.gg/nW6x9EN)`)
+			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }))
+			.setTimestamp();
+
+		message.channel.send(embed);
+	}
+
+};
