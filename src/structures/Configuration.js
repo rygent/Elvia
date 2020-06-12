6d9cea9315a632000d4587864a9f6e01af414c85
XRzky <marizky65@gmail.com>
2020-06-12 08:05:33 +0700

    Configuration: remove colors CUSTOM
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/structures/Configuration.js
+++ src/structures/Configuration.js
@@ -29,8 +29,7 @@ module.exports = {
 		LIGHT_GREY: 'bcc0c0',
 		DARK_NAVY: '2c3e50',
 		LUMINOUS_VIVID_PINK: 'fd0061',
-		DARK_VIVID_PINK: 'bc0057',
-		CUSTOM: 'ff4654'
+		DARK_VIVID_PINK: 'bc0057'
 	},
 	Emojis: {
 		ONLINE: '<:online:712397262256472075>',
