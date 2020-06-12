7072d260d3f0e46ae94dd53c9e558f4f2ade4cc8
XRzky <marizky65@gmail.com>
2020-06-13 04:15:32 +0700

    commands: added restart
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/owner/restart.js
@@ -0,0 +1,24 @@
+const Command = require('../../../structures/Command.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'restart',
+			aliases: ['reboot'],
+			description: 'If running under PM2, the bot will restart.',
+			category: 'owner',
+			ownerOnly: true
+		});
+	}
+
+	async run(message) {
+		try {
+			await message.channel.send('Rebooting, please wait...');
+			process.exit(1);
+		} catch (err) {
+			message.channel.send(`ERROR: ${err.message}`);
+		}
+	}
+
+};
