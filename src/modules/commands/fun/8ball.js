634dca145144dc56614fe391374646f2a3397f38
XRzky <marizky65@gmail.com>
2020-06-13 10:06:12 +0700

    commands: add 8ball command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/fun/8ball.js
@@ -0,0 +1,41 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+const eightBall = require('../../../assets/json/8ball.json');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: '8ball',
+			aliases: ['8-ball', 'eightball', 'fortune'],
+			description: 'Returns an answer to any question!',
+			category: 'fun',
+			usage: '<question>',
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS']
+		});
+	}
+
+	async run(message, args) {
+		if (!args[1]) {
+			message.channel.send('Please provide a question for me to answer.');
+			return;
+		}
+
+		let roleColor;
+		if (!message.guild) {
+			roleColor = Colors.DEFAULT;
+		} else {
+			roleColor = message.guild.me.roles.highest.hexColor;
+		}
+
+		const embed = new MessageEmbed()
+			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+			.setTitle(`🎱 ${args.slice(0).join(' ')}`)
+			.setDescription(`❯  ${eightBall[Math.floor(Math.random() * eightBall.length).toString(10)]}`)
+			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
+
+		message.channel.send(embed);
+	}
+
+};
