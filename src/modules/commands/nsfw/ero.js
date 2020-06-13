07d5592d73f5e42b5ca3be6ecf93f2ea3e5cd874
XRzky <marizky65@gmail.com>
2020-06-14 02:26:40 +0700

    nsfw: add ero command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/nsfw/ero.js
@@ -0,0 +1,38 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+const { handleValidation } = require('../../../utils/NsfwHandling.js');
+const Client = require('nekos.life');
+const { nsfw } = new Client();
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'ero',
+			aliases: ['erotic'],
+			description: 'Posts a random erotic picture. Warning this commands for 18+',
+			category: 'nsfw',
+			guildOnly: true,
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS'],
+			nsfw: true
+		});
+	}
+
+	async run(message) {
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		message.channel.startTyping(true);
+		// eslint-disable-next-line no-sequences
+		nsfw[['holoEro', 'eroFeet', 'ero', 'eroKitsune', 'eroKemonomimi', 'eroNeko', 'eroYuri'].random()]().then(async res => {
+			const embed = new MessageEmbed()
+				.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+				.setImage(res.url)
+				.setFooter(`Responded in ${this.client.functions.responseTime(message)} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }));
+
+			handleValidation(embed, message);
+		});
+		message.channel.stopTyping(true);
+	}
+
+};
