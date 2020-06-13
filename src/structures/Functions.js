99af2fd9c947aa63622c173206e7c61e02ab4844
XRzky <marizky65@gmail.com>
2020-06-13 19:09:00 +0700

    Functions: add promptMessage function
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/structures/Functions.js
+++ src/structures/Functions.js
@@ -3,5 +3,16 @@ module.exports = {
 	responseTime: function (message) {
 		const time = Date.now() - message.createdTimestamp;
 		return `${time || 0} ms`;
+	},
+	promptMessage: async function (message, author, time, validReactions) {
+		time *= 1000;
+
+		for (const reaction of validReactions) await message.react(reaction);
+
+		const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
+
+		return message
+			.awaitReactions(filter, { max: 1, time: time })
+			.then(collected => collected.first() && collected.first().emoji.name);
 	}
 };
