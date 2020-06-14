c6f2cbac40106301ba38fff20ecffc4b8d98950d
XRzky <marizky65@gmail.com>
2020-06-14 21:00:40 +0700

    action: add kemonomimi command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/action/kemonomimi.js
@@ -0,0 +1,42 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+const Client = require('nekos.life');
+const { sfw } = new Client();
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'kemonomimi',
+			aliases: ['kemono', 'kemonomi'],
+			description: 'Gives you a kemonomimi!',
+			category: 'action',
+			usage: '[Mention]',
+			guildOnly: true,
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS']
+		});
+	}
+
+	async run(message) {
+		const mention = message.mentions.users.first();
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		message.channel.startTyping(true);
+		sfw.kemonomimi().then(async res => {
+			const embed = new MessageEmbed()
+				.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+				.setDescription(`Have some animal humans ${message.author}.`)
+				.setImage(res.url)
+				.setFooter(`Responded in ${this.client.functions.responseTime(message)} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }));
+
+			if (mention && mention !== message.author) {
+				embed.setDescription(`${message.author} gave some animal humans to ${mention}.`);
+			}
+
+			message.channel.send(embed);
+		});
+		message.channel.stopTyping(true);
+	}
+
+};
