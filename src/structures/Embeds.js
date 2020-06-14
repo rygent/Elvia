10f98146090c64c323b88e94923f2f0936f4f80f
XRzky <marizky65@gmail.com>
2020-06-15 01:48:08 +0700

    Embeds: Remove unnecessary custom embeds
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/structures/Embeds.js
+++ src/structures/Embeds.js
@@ -4,20 +4,6 @@ const { Colors } = require('./Configuration.js');
 const { responseTime } = require('./Functions.js');
 
 module.exports = {
-	generals: async function (type, message, args) {
-		const roleColor = message.guild.me.roles.highest.hexColor;
-
-		const embed = new MessageEmbed()
-			.setColor(roleColor === '#000000' ? Colors.CUSTOM : roleColor)
-			.setFooter(`Responded in ${responseTime(message)}`, message.author.avatarURL({ dynamic: true }))
-			.setTimestamp();
-		switch (type) {
-			default: {
-				embed.setDescription(`**${message.author.tag}**, ${args}`);
-			}
-		}
-		message.channel.send(embed);
-	},
 	errors: async function (type, message, args) {
 		const embed = new MessageEmbed()
 			.setColor(Colors.RED)
