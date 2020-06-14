cf56dffe99ac1175e32a3fc8117b761c30d2d698
XRzky <marizky65@gmail.com>
2020-06-14 21:01:37 +0700

    action: add smug command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/action/smug.js
@@ -0,0 +1,42 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+const Client = require('nekos.life');
+const { sfw } = new Client();
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'smug',
+			aliases: [],
+			description: 'Gives you a smug!',
+			category: 'action',
+			usage: '[Mention]',
+			guildOnly: true,
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS']
+		});
+	}
+
+	async run(message) {
+		const mention = message.mentions.users.first();
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		message.channel.startTyping(true);
+		sfw.smug().then(async res => {
+			const embed = new MessageEmbed()
+				.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+				.setDescription(`${message.author} is looking a bit smug.`)
+				.setImage(res.url)
+				.setFooter(`Responded in ${this.client.functions.responseTime(message)} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }));
+
+			if (mention && mention !== message.author) {
+				embed.setDescription(`${message.author} is being smug against ${mention}.`);
+			}
+
+			message.channel.send(embed);
+		});
+		message.channel.stopTyping(true);
+	}
+
+};
