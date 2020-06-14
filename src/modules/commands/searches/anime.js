de2d1f67af48254f7baad49f0ba96d0a06014224
XRzky <marizky65@gmail.com>
2020-06-15 04:10:12 +0700

    searches: add anime command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/searches/anime.js
@@ -0,0 +1,53 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+const { stripIndents } = require('common-tags');
+const malScraper = require('mal-scraper');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'anime',
+			aliases: ['mal', 'myanimelist'],
+			description: 'Searches information from my anime list.',
+			category: 'searches',
+			usage: '<query>',
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS']
+		});
+	}
+
+	async run(message, args) {
+		const query = args.join(' ').trim();
+		if (!query) {
+			message.channel.send('Please specify an anime to search!');
+		}
+
+		malScraper.getInfoFromName(query).then(data => {
+			const embed = new MessageEmbed()
+				.setColor(Colors.MAL)
+				.setAuthor('MyAnimeList Search Engine', 'https://i.imgur.com/QABhOrL.png', 'https://myanimelist.net/')
+				.setTitle(data.title)
+				.setURL(data.url)
+				.setThumbnail(data.picture)
+				.setDescription(data.synopsis)
+				.addField('__Details__', stripIndents`
+                    ***English title:*** ${data.englishTitle || 'Unknown'}
+                    ***Japanese title:*** ${data.japaneseTitle}
+                    ***Synonyms:*** ${data.synonyms || 'Unknown'}
+                    ***Score:*** ${data.score} (${data.scoreStats})
+                    ***Genres:*** ${data.genres.join(', ').toString()}
+                    ***Rating:*** ${data.rating}
+                    ***Source:*** ${data.source}
+                    ***Type:*** ${data.type}
+                    ***Episodes:*** ${data.episodes}
+                    ***Duration:*** ${data.duration}
+                    ***Aired:*** ${data.aired}
+                    ***Status:*** ${data.status}`)
+				.setFooter(`Responded in ${this.client.functions.responseTime(message)} | Powered by MyAnimeList`, message.author.avatarURL({ dynamic: true }));
+
+			message.channel.send(embed);
+		}).catch(() => message.channel.send('Can\'t search, make sure the anime title is correct'));
+	}
+
+};
--- src/structures/Configuration.js
+++ src/structures/Configuration.js
@@ -30,7 +30,8 @@ module.exports = {
 		DARK_NAVY: '2c3e50',
 		LUMINOUS_VIVID_PINK: 'fd0061',
 		DARK_VIVID_PINK: 'bc0057',
-		INSTAGRAM: 'e1306c'
+		INSTAGRAM: 'e1306c',
+		MAL: '2e51a2'
 	},
 	Emojis: {
 		SUCCESS: '<:success:721207406431830056>',
