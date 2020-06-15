a2e2379eabea27106eaf7ab92de9c8ae7ae8e7d1
XRzky <marizky65@gmail.com>
2020-06-15 08:54:50 +0700

    information: remove timestamp
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/information/avatar.js
+++ src/modules/commands/information/avatar.js
@@ -33,8 +33,7 @@ module.exports = class extends Command {
 			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
 			.setTitle('🖼️ Avatars')
 			.setImage(user.displayAvatarURL({ format: 'png', dynamic: true, size: 512 }))
-			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }))
-			.setTimestamp();
+			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
 
 		if (message.content.includes('-hd')) {
 			avatarEmbed.addField(`${user.tag}`, stripIndents`
--- src/modules/commands/information/server.js
+++ src/modules/commands/information/server.js
@@ -76,8 +76,7 @@ module.exports = class extends Command {
                 ***Booster:*** ${message.guild.premiumSubscriptionCount}`, true)
 			.addField(`__Roles [${message.guild.roles.cache.filter(fil => fil.name !== '@everyone').size}]__`, stripIndents`
                 ${message.guild.roles.cache.sort((a, b) => b.position - a.position).filter(fil => fil.name !== '@everyone').map(re => re.name).join(', ')}`)
-			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }))
-			.setTimestamp();
+			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
 
 		message.channel.send(embed);
 	}
