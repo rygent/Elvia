123498e13486fc40cb13358692a074d2ae9225cd
XRzky <marizky65@gmail.com>
2020-07-14 02:23:03 +0700

    Guild: add new schema
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/structures/database/models/Guild.js
+++ src/structures/database/models/Guild.js
@@ -4,5 +4,40 @@ const { prefix } = require('../../Configuration.js');
 
 module.exports = mongoose.model('Guild', new Schema({
 	id: { type: String },
-	prefix: { type: String, default: prefix }
+	membersData: { type: Object, default: {} },
+	members: [{ type: Schema.Types.ObjectId, ref: 'Member' }],
+	prefix: { type: String, default: prefix },
+	plugins: { type: Object, default: {
+		welcome: {
+			enabled: false,
+			message: null,
+			channel: null,
+			withImage: null
+		},
+		goodbye: {
+			enabled: false,
+			message: null,
+			channel: null,
+			withImage: null
+		},
+		autorole: {
+			enabled: false,
+			role: null
+		},
+		automod: {
+			enabled: false,
+			ignored: []
+		},
+		warnsSanctions: {
+			kick: false,
+			ban: false
+		},
+		suggestions: false,
+		modlogs: false,
+		reports: false,
+		logs: false
+	} },
+	casesCount: { type: Number, default: 0 },
+	ignoredChannels: { type: Array, default: [] },
+	autoDeleteModCommands: { type: Boolean, default: false }
 }));
