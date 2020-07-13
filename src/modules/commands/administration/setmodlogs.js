8904aa0308e4eb8dc63f8f220cd64ece2e5fe252
XRzky <marizky65@gmail.com>
2020-07-14 02:23:43 +0700

    administration: add setmodlogs command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/administration/setmodlogs.js
@@ -0,0 +1,28 @@
+const Command = require('../../../structures/Command.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Define the log channel!',
+			category: 'administration',
+			usage: '<mention-channel>',
+			memberPerms: ['MANAGE_GUILD'],
+			clientPerms: ['SEND_MESSAGES', 'MANAGE_GUILD'],
+			cooldown: 3000
+		});
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	async run(message, args, data) {
+		const channel = message.mentions.channels.filter((ch) => ch.type === 'text' && ch.guild.id === message.guild.id).first() || message.channel;
+		data.guild.plugins.modlogs = channel.id;
+		data.guild.markModified('plugins.modlogs');
+		data.guild.save();
+
+		// Send success message
+		message.channel.send(`Moderation logs channel defined on <#${channel.id}> !`);
+	}
+
+};
