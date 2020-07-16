61e41ea8b03d86b74b82351fcc8af08dbad6639f
XRzky <marizky65@gmail.com>
2020-07-16 11:16:48 +0700

    addemote: create embed for message reply
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/administration/addemote.js
+++ src/modules/commands/administration/addemote.js
@@ -4,35 +4,29 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			name: 'addemote',
 			aliases: [],
 			description: 'Add an emoji to the server!',
 			category: 'administration',
 			usage: '<URL> <name>',
-			memberPerms: ['ADMINISTRATOR'],
-			clientPerms: ['SEND_MESSAGES', 'MANAGE_EMOJIS']
+			memberPerms: ['MANAGE_GUILD', 'MANAGE_EMOJIS'],
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS', 'MANAGE_GUILD', 'MANAGE_EMOJIS'],
+			cooldown: 3000
 		});
 	}
 
-	async run(message, args) {
-		const url = args[0];
-		if (!url) {
-			message.channel.send('Please indicate the url of the emoji!');
-			return;
+	/* eslint-disable consistent-return */
+	async run(message, [url, name]) {
+		if (!url || !url.startsWith('http')) {
+			return this.client.embeds.common('commonError', message, 'Please provide a URL of the emoji!');
 		}
 
-		const name = args[1];
 		if (!name) {
-			message.channel.send('Please indicate the name of the emoji!');
-			return;
+			return this.client.embeds.common('commonError', message, 'Please provide a name of the emoji!');
 		}
 
 		message.guild.emojis.create(url, name).then(emote => {
-			message.channel.send(`Emoji **${emote.name}** added to the server! String: \`${emote.toString()}\``);
-		}).catch(() => {
-			message.channel.send('The URL to the image is invalid or you have no more space on your Discord!');
-			return;
-		});
+			this.client.embeds.common('commonSuccess', message, `Emoji **${emote.name}** added to the server! String: \`${emote.toString()}\``);
+		}).catch(() => this.client.embeds.common('commonError', message, 'The URL image is invalid or you don\'t have more space on your server!'));
 	}
 
 };
