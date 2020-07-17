58d21d47a2f8dfbc2bd09f51b844ada342c7599f
XRzky <marizky65@gmail.com>
2020-07-18 06:04:01 +0700

    setprefix: change reply message using embed
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/administration/setprefix.js
+++ src/modules/commands/administration/setprefix.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
 			category: 'administration',
 			usage: '<prefix>',
 			memberPerms: ['MANAGE_GUILD'],
-			clientPerms: ['SEND_MESSAGES', 'MANAGE_GUILD'],
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS', 'MANAGE_GUILD'],
 			cooldown: 3000
 		});
 	}
@@ -18,14 +18,14 @@ module.exports = class extends Command {
 	async run(message, [prefix], data) {
 		const banned = ['#', '@', '`'];
 
-		if (!prefix) return message.channel.send('You must provide a valid prefix to set.');
-		if (banned.includes(prefix)) return message.channel.send('This prefix is unavailable. It may be a markdown character.');
-		if (prefix.length > 5) return message.channel.send('The prefix must not exceed 5 characters!');
+		if (!prefix) return this.client.embeds.common('commonError', message, 'You must provide a valid prefix to set.');
+		if (banned.includes(prefix)) return this.client.embeds.common('commonError', message, 'This prefix is unavailable. It may be a markdown character.');
+		if (prefix.length > 5) return this.client.embeds.common('commonError', message, 'The prefix must not exceed 5 characters!');
 
 		data.guild.prefix = prefix;
 		data.guild.save();
 
-		message.channel.send(`The prefix has been changed to "**${prefix}**". If you ever forget the prefix just tag me!`);
+		this.client.embeds.common('commonSuccess', message, `The prefix has been changed to "**${prefix}**". If you ever forget the prefix just tag me!`);
 	}
 
 };
