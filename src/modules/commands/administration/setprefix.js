f25bdfee5c263f91fc31cdb59a38071df546a8d0
XRzky <marizky65@gmail.com>
2020-07-18 05:55:33 +0700

    administration: rename prefix to setprefix
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/administration/setprefix.js
@@ -0,0 +1,31 @@
+const Command = require('../../../structures/Command.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pfx', 'prefixes', 'guildprefix'],
+			description: 'Displays prefix or set the server\'s prefix.',
+			category: 'administration',
+			usage: '<prefix>',
+			memberPerms: ['MANAGE_GUILD'],
+			clientPerms: ['SEND_MESSAGES', 'MANAGE_GUILD'],
+			cooldown: 3000
+		});
+	}
+
+	// eslint-disable-next-line consistent-return
+	async run(message, [prefix], data) {
+		const banned = ['#', '@', '`'];
+
+		if (!prefix) return message.channel.send('You must provide a valid prefix to set.');
+		if (banned.includes(prefix)) return message.channel.send('This prefix is unavailable. It may be a markdown character.');
+		if (prefix.length > 5) return message.channel.send('The prefix must not exceed 5 characters!');
+
+		data.guild.prefix = prefix;
+		data.guild.save();
+
+		message.channel.send(`The prefix has been changed to "**${prefix}**". If you ever forget the prefix just tag me!`);
+	}
+
+};
