a6ef4e09d9e6f8a1a195185646009d1cb1c2dbac
XRzky <marizky65@gmail.com>
2020-07-19 03:57:13 +0700

    setmodlogs: change reply message using embed
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/administration/setmodlogs.js
+++ src/modules/commands/administration/setmodlogs.js
@@ -7,22 +7,22 @@ module.exports = class extends Command {
 			aliases: [],
 			description: 'Define the log channel!',
 			category: 'administration',
-			usage: '<mention-channel>',
+			usage: '[mention-channel]',
 			memberPerms: ['MANAGE_GUILD'],
-			clientPerms: ['SEND_MESSAGES', 'MANAGE_GUILD'],
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS', 'MANAGE_GUILD'],
 			cooldown: 3000
 		});
 	}
 
 	// eslint-disable-next-line no-unused-vars
 	async run(message, args, data) {
-		const channel = message.mentions.channels.filter((ch) => ch.type === 'text' && ch.guild.id === message.guild.id).first() || message.channel;
+		const channel = message.mentions.channels.filter(ch => ch.type === 'text' && ch.guild.id === message.guild.id).first() || message.channel;
+
 		data.guild.plugins.modlogs = channel.id;
 		data.guild.markModified('plugins.modlogs');
 		data.guild.save();
 
-		// Send success message
-		message.channel.send(`Moderation logs channel defined on <#${channel.id}> !`);
+		this.client.embeds.common('commonSuccess', message, `Moderation logs channel defined on <#${channel.id}> !`);
 	}
 
 };
