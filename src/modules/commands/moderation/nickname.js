247b747b40ba40b77711f55328ce2e03c9ec079a
XRzky <marizky65@gmail.com>
2020-08-11 05:25:22 +0700

    nickname: revamp the appearance to make it better
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/moderation/nickname.js
+++ src/modules/commands/moderation/nickname.js
@@ -17,17 +17,17 @@ module.exports = class extends Command {
 	/* eslint-disable consistent-return */
 	async run(message, [target, ...args]) {
 		const member = message.mentions.members.last() || message.guild.members.cache.get(target);
-		if (!member) return message.channel.send('Please mention a valid member!');
+		if (!member) return this.client.embeds.common('commonError', message, 'Please mention a valid member!');
 		if (message.guild.member(message.author).roles.highest.position <= message.guild.member(member).roles.highest.position) {
-			return message.channel.send(`You can't change **${member.user.username}**'s nickname! Their position is higher than you!`);
+			return this.client.embeds.common('commonError', message, `You can't change **${member.user.username}**'s nickname! Their position is higher than you!`);
 		}
-		if (!member.manageable) return message.channel.send(`I can't change **${member.user.username}**'s nickname! Their role is higher than mine!`);
+		if (!member.manageable) return this.client.embeds.common('commonError', message, `I can't change **${member.user.username}**'s nickname! Their role is higher than mine!`);
 
 		const nickname = args.join(' ');
 
 		return await nickname !== 'clear' ? member.setNickname(nickname).then(() =>
 			// eslint-disable-next-line max-len
-			message.channel.send(`The nickname **${nickname}** has been assigned to **${member.user.username}**!`)) : member.setNickname('').then(() => message.channel.send(`**${member.displayName}**'s nickname has been cleared!`));
+			this.client.embeds.common('commonSuccess', message, `The nickname **${nickname}** has been assigned to **${member.user.username}**!`)) : member.setNickname('').then(() => this.client.embeds.common('commonSuccess', message, `**${member.displayName}**'s nickname has been cleared!`));
 	}
 
 };
