d78369864d8e2ca721449d942e710031ab1f59d4
XRzky <marizky65@gmail.com>
2020-08-21 03:36:21 +0700

    ready: revamp for better look
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/events/ready.js
+++ src/modules/events/ready.js
@@ -12,15 +12,14 @@ module.exports = class extends Event {
 	}
 
 	async run() {
-		const timestamp = `${moment().format('MMM D, YYYY HH:mm:ss')}`;
 		console.log([
-			`Logged in as ${chalk.redBright(`${this.client.user.tag}`)}`,
-			`Loaded ${this.client.commands.size.formatNumber()} commands & ${this.client.events.size.formatNumber()} events!`,
+			`[${chalk.grey('INFO')}] Logged in as ${chalk.redBright(`${this.client.user.tag}`)}`,
+			`[${chalk.grey('INFO')}] Loaded ${this.client.commands.size.formatNumber()} commands & ${this.client.events.size.formatNumber()} events!`,
 			// eslint-disable-next-line max-len
-			`Ready in ${this.client.guilds.cache.size.formatNumber()} guilds on ${this.client.channels.cache.size.formatNumber()} channels, for a total of ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).formatNumber()} users.`
+			`[${chalk.grey('INFO')}] Ready in ${this.client.guilds.cache.size.formatNumber()} guilds on ${this.client.channels.cache.size.formatNumber()} channels, for a total of ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).formatNumber()} users.`
 		].join('\n'));
 		process.stdout.write(`[${chalk.greenBright('BOOT')}] Connected to Discord API!\n`);
-		process.stdout.write(`[${chalk.greenBright('BOOT')}] Booted up in ${chalk.blueBright(`${(Date.now() - global.startTime) / 1000}s`)} on ${chalk.blueBright(`${timestamp}`)}\n`);
+		process.stdout.write(`[${chalk.greenBright('BOOT')}] Booted up on ${chalk.blueBright(`${moment().format('MMM D, YYYY HH:mm:ss')}`)}\n`);
 
 		const activities = [
 			`${this.client.guilds.cache.size.formatNumber()} servers`,
