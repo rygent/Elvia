ca8c779675cc535d9f69e7657ce2c2177d600a31
XRzky <marizky65@gmail.com>
2020-08-21 04:54:18 +0700

    invite: fix error link
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/utility/invite.js
+++ src/modules/commands/utility/invite.js
@@ -20,7 +20,7 @@ module.exports = class extends Command {
 		const embed = new MessageEmbed()
 			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
 			.setTitle('Click Here to add me to your Server!')
-			.setURL('https://discordapp.com/oauth2/authorize?&client_id=${this.client.user.id}&scope=bot&permissions=1584786551')
+			.setURL(`https://discordapp.com/oauth2/authorize?&client_id=${this.client.user.id}&scope=bot&permissions=1584786551`)
 			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
 
 		message.channel.send(embed);
