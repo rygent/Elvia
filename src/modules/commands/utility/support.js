b0ebe5618da7e0e7fc02e936d10695dc5e0b3a54
XRzky <marizky65@gmail.com>
2020-08-21 04:01:54 +0700

    commands: add support command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/utility/support.js
@@ -0,0 +1,29 @@
+const Command = require('../../../structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../structures/Configuration.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Gives you the support server invite link!',
+			category: 'utility',
+			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS'],
+			cooldown: 5000
+		});
+	}
+
+	async run(message) {
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		const embed = new MessageEmbed()
+			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+			.setTitle('Click Here to join Support Server')
+			.setURL('https://discord.gg/nW6x9EN')
+			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
+
+		message.channel.send(embed);
+	}
+
+};
