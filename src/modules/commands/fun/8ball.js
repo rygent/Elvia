3ae73534970b889cd19376a28fa27914a5576fd4
XRzky <marizky65@gmail.com>
2020-10-23 09:30:01 +0700

    8ball: add answer level
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/fun/8ball.js
+++ src/modules/commands/fun/8ball.js
@@ -8,11 +8,9 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
 			aliases: ['8-ball', 'eightball', 'fortune'],
-			description: 'Returns an answer to any question!',
+			description: 'A command decides your fate with an 8-ball, obviously 8-balls aren\'t real you dingus.',
 			category: 'fun',
-			usage: '<question>',
-			memberPerms: [],
-			clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS'],
+			usage: '<your question>',
 			cooldown: 3000
 		});
 	}
@@ -20,16 +18,19 @@ module.exports = class extends Command {
 	/* eslint-disable consistent-return */
 	async run(message, args) {
 		if (!args[1]) {
-			return this.client.embeds.common('commonError', message, 'Please provide a question for me to answer.');
+			return this.client.embeds.common('commonError', message, 'Provide a question for the 8-ball.');
 		}
+
+		const RatingArray = ['Low', 'Medium', 'High'];
+		const Choice = eightBall[Math.floor(Math.random() * eightBall.length).toString(10)];
 
 		const roleColor = message.guild.me.roles.highest.hexColor;
 
 		const embed = new MessageEmbed()
 			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
 			.setTitle(`🎱 ${args.slice(0).join(' ')}`)
-			.setDescription(`❯  ${eightBall[Math.floor(Math.random() * eightBall.length).toString(10)]}`)
-			.setFooter(`Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
+			.setDescription(`❯  ${Choice.Message}`)
+			.setFooter(`Rate: ${RatingArray[Choice.Rating]} | Responded in ${this.client.functions.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
 
 		message.channel.send(embed);
 	}
