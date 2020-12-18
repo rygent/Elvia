197df8e1b6535102bedfcf3b8a112c56527b7957
XRzky <marizky65@gmail.com>
2020-12-18 17:14:16 +0700

    Added Invite command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/Modules/Commands/Utilities/Invite.js
@@ -0,0 +1,27 @@
+const Command = require('../../../Structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../Structures/Configuration.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Gives you the bot invite link!',
+			category: 'Utilities'
+		});
+	}
+
+	async run(message) {
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		const embed = new MessageEmbed()
+			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+			.setTitle('Click Here to add me to your Server!')
+			.setURL(`https://discordapp.com/oauth2/authorize?&client_id=${this.client.user.id}&scope=bot&permissions=1584786551`)
+			.setFooter(`Responded in ${this.client.utils.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
+
+		message.channel.send(embed);
+	}
+
+};
