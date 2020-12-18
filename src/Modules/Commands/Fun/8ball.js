8782a6a7de44628271c547b9d01724acf77f48d8
XRzky <marizky65@gmail.com>
2020-12-18 16:57:18 +0700

    Added 8ball command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/Modules/Commands/Fun/8ball.js
@@ -0,0 +1,36 @@
+const Command = require('../../../Structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../Structures/Configuration.js');
+const answers = require('../../../../assets/json/8ball.json');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['8-ball', 'eightball', 'fortune'],
+			description: 'A command decides your fate with an 8-ball, obviously 8-balls aren\'t real you dingus.',
+			category: 'Fun',
+			usage: '<question>'
+		});
+	}
+
+	async run(message, args) {
+		if (!args[1]) {
+			return message.reply('Provide a question for the 8-ball.');
+		}
+
+		const RatingArray = ['Low', 'Medium', 'High'];
+		const Choice = answers[Math.floor(Math.random() * answers.length).toString(10)];
+
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		const embed = new MessageEmbed()
+			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+			.setTitle(`🎱 ${args.slice(0).join(' ')}`)
+			.setDescription(`❯  ${Choice.Message}`)
+			.setFooter(`Requested by ${message.author.tag} | Rate: ${RatingArray[Choice.Rating]}`, message.author.avatarURL({ dynamic: true }));
+
+		return message.channel.send(embed);
+	}
+
+};
