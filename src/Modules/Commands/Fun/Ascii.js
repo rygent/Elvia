731b24e769e2ba270666dde33795d963d38e0548
XRzky <marizky65@gmail.com>
2020-12-18 16:58:12 +0700

    Added Ascii command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/Modules/Commands/Fun/Ascii.js
@@ -0,0 +1,22 @@
+const Command = require('../../../Structures/Command.js');
+const figlet = require('util').promisify(require('figlet'));
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Turn your text into ascii characters!',
+			category: 'Fun',
+			usage: '<text>'
+		});
+	}
+
+	async run(message, args) {
+		const text = args.join(' ');
+		if (!text || text.length > 20) return message.reply('Please enter a valid text (less than 20 characters)!');
+
+		return message.channel.send(await figlet(text), { code: true });
+	}
+
+};
