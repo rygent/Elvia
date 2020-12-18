9d6feaa5d7270f77928ad4fe3a6cfdf1d20ac4ad
XRzky <marizky65@gmail.com>
2020-12-18 17:08:41 +0700

    Added Execute command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/Modules/Commands/Developer/Execute.js
@@ -0,0 +1,23 @@
+const Command = require('../../../Structures/Command.js');
+const { exec } = require('child_process');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['exec'],
+			description: 'Executes commands in the console.',
+			category: 'Developer',
+			usage: '<query>',
+			ownerOnly: true
+		});
+	}
+
+	async run(message, args) {
+		exec(args.join(' '), (error, stdout) => {
+			const response = stdout || error;
+			message.channel.send(response, { split: true, code: true });
+		});
+	}
+
+};
