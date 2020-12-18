74542eb4d4f5c00b57b09c272cb1d9cde0990a1c
XRzky <marizky65@gmail.com>
2020-12-18 16:40:34 +0700

    Utilities: Added Ping command
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/Modules/Commands/Utilities/Ping.js
@@ -0,0 +1,36 @@
+const Command = require('../../../Structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Colors } = require('../../../Structures/Configuration.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pong'],
+			description: 'Displays bot latency & API response times.',
+			category: 'Utilities'
+		});
+	}
+
+	async run(message) {
+		const msg = await message.channel.send('Pinging...');
+		const latency = Math.round(msg.createdTimestamp - message.createdTimestamp);
+
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		const embed = new MessageEmbed()
+			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+			.setDescription([
+				`💓 ***Heartbeat:*** \`${Math.round(this.client.ws.ping)}ms\``,
+				`⏱️ ***Latency:*** \`${latency}ms\``
+			].join('\n'))
+			.setFooter(`Requested by ${message.author.tag}`, message.author.avatarURL({ dynamic: true }));
+
+		if (latency <= 0) {
+			embed.setDescription('Please try again later');
+		}
+
+		msg.edit('\u200B', embed);
+	}
+
+};
