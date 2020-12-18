08690bc734c4b678ba53b533ced1107d42429720
XRzky <marizky65@gmail.com>
2020-12-18 16:56:12 +0700

    Added Logging Message Event
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/Modules/Events/message/messageDelete.js
@@ -0,0 +1,33 @@
+const Event = require('../../../Structures/Event.js');
+const ElainaEmbed = require('../../../Structures/ElainaEmbed.js');
+const { Colors } = require('../../../Structures/Configuration.js');
+
+module.exports = class extends Event {
+
+	async run(message) {
+		if (!message.guild || message.author.bot) return;
+
+		const sendChannel = message.guild.channels.cache.find(ch => ch.name === 'audit-logs');
+		if (!sendChannel) return;
+
+		const attachments = message.attachments.size ? message.attachments.map(attachment => attachment.proxyURL) : null;
+		const roleColor = message.guild.me.roles.highest.hexColor;
+
+		const embed = new ElainaEmbed()
+			.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
+			.setTitle('Message Delete')
+			.setDescription([
+				`***Message ID:*** \`${message.id}\``,
+				`***Channel:*** ${message.channel}`,
+				`***Author:*** ${message.member.displayName}`,
+				`${attachments ? `***Attachments:*** ${attachments.join('\n')}` : ''}`
+			].join('\n'))
+			.setFooter(`Powered by ${this.client.user.username}`, this.client.user.avatarURL({ dynamic: true }));
+		if (message.content.length) {
+			embed.splitFields(`***Deleted Message:*** ${message.content}`);
+		}
+
+		if (sendChannel) sendChannel.send(embed);
+	}
+
+};
