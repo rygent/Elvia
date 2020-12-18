7181100dda7005cb9f3e4a0538b6a6ec0f71aa2d
XRzky <marizky65@gmail.com>
2020-12-18 16:23:06 +0700

    Removed old stuff
    Signed-off-by: XRzky <marizky65@gmail.com>

--- database/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-module.exports = mongoose.model('User', new Schema({
-	id: { type: String },
-	registeredAt: { type: Number, default: Date.now() },
-	afk: { type: String, default: null },
-	reminds: { type: Array, default: [] }
-}));
--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const EvoraClient = require('./structures/EvoraClient.js');
-const config = require('./structures/Configuration.js');
-
-const client = new EvoraClient(config);
-
-client.start();
