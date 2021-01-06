940be8ee988f9b4c06dc6aa78b757f7c9500c69e
XRzky <marizky65@gmail.com>
2021-01-06 16:42:29 +0700

    Kick: Replace dm message
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/Modules/Commands/Moderation/Kick.js
+++ src/Modules/Commands/Moderation/Kick.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
 			aliases: ['boot'],
 			description: 'Kicks the given user and DMs them the reason!',
 			category: 'Moderation',
-			usage: '<Mention | ID> <reason>',
+			usage: '<@member> <reason>',
 			userPerms: ['KICK_MEMBERS'],
 			clientPerms: ['KICK_MEMBERS'],
 			cooldown: 3000
@@ -19,42 +19,29 @@ module.exports = class extends Command {
 	/* eslint-disable consistent-return */
 	async run(message, [target, ...args]) {
 		const member = message.mentions.members.last() || message.guild.members.cache.get(target);
-		if (!member) return message.quote('Please mention a valid member!');
+		if (!member) return message.quote('Please specify a valid member to ban!');
 
 		const guildData = await this.client.findOrCreateGuild({ id: message.guild.id });
 		const memberData = await this.client.findOrCreateMember({ id: member.id, guildID: message.guild.id });
 
-		if (member.id === this.client.user.id) return message.quote('Please don\'t kick me...!');
 		if (member.id === message.author.id) return message.quote('You can\'t kick yourself!');
-		if (member.id === this.client.owner) return message.quote('I can\'t kick my master');
 		if (message.guild.member(message.author).roles.highest.position <= message.guild.member(member).roles.highest.position) {
-			return message.quote(`You can't kick **${member.user.username}**! Their position is higher than you!`);
+			return message.quote('You can\'t kick a member who has an higher or equal role hierarchy to yours!');
 		}
 
 		const reason = args.join(' ');
-		if (!reason) return message.quote('Please provide a reason');
+		if (!reason) return message.quote('Please enter a reason!');
 
 		if (!message.guild.member(member).kickable) {
 			return message.quote(`I can't kick **${member.user.username}**! Their role is higher than mine!`);
 		}
 
 		if (!member.user.bot) {
-			const embed = new MessageEmbed()
-				.setColor(Colors.ORANGE)
-				.setTitle('👢 Kicked')
-				.setDescription([
-					`Hello <@${member.id}>,`,
-					`You have just been kicked out from **${message.guild.name}** by **${message.author.tag}**`,
-					`Reason: **${reason}**\n`,
-					`__*Please make sure you always follow the rules, because not doing so can result in punishment.*__`
-				].join('\n'))
-				.setFooter(`Moderation system powered by ${this.client.user.username}`, this.client.user.avatarURL({ dynamic: true }));
-
-			member.send(embed);
+			await member.send(`Hello <@${member.id}>,\nYou've just been kicked from **${message.guild.name}** by **${message.author.tag}** because of **${reason}**!`);
 		}
 
 		member.kick(`${message.author.tag}: ${reason}`).then(() => {
-			message.quote(`**${member.user.username}**, has been kicked out.`);
+			message.quote(`**${member.user.username}** has just been kicked from **${message.guild.name}** by **${message.author.tag}** because of **${reason}**!`);
 
 			guildData.casesCount++;
 			guildData.save();
@@ -81,8 +68,8 @@ module.exports = class extends Command {
 					.setColor(roleColor === '#000000' ? Colors.DEFAULT : roleColor)
 					.setAuthor(`Moderation: Kick | Case #${guildData.casesCount}`, member.user.avatarURL({ dynamic: true }))
 					.setDescription([
-						`***User:*** ${member.user.tag} (${member.user.id})`,
-						`***Moderator:*** ${message.author.tag} (${message.author.id})`,
+						`***User:*** ${member.user.tag} (\`${member.user.id}\`)`,
+						`***Moderator:*** ${message.author.tag} (\`${message.author.id}\`)`,
 						`***Reason:*** ${reason}`
 					].join('\n'))
 					.setFooter(`Moderation system powered by ${this.client.user.username}`, this.client.user.avatarURL({ dynamic: true }));
