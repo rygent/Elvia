d6b4173121aff9b882f4dfb19d215c683007f308
Rygent <marizky65@gmail.com>
2021-06-18 07:59:50 +0700

    Message: Improve answer to make it easier to understand
    Signed-off-by: Rygent <marizky65@gmail.com>

--- src/Modules/Events/message/message.js
+++ src/Modules/Events/message/message.js
@@ -14,7 +14,10 @@ module.exports = class extends Event {
 		const customPrefix = guildData ? guildData.prefix : this.client.prefix;
 
 		if (message.content.match(mentionRegex)) {
-			return message.reply(`Hello **${message.author.username}**, my prefix on this server is \`${customPrefix}\`.\nUse \`${customPrefix}help\` to get the list of the commands!`);
+			return message.reply([
+				`Hi, my prefix for this guild is \`${customPrefix}\`.`,
+				`Use \`${customPrefix}help\` to get a list of commands!`
+			].join('\n'));
 		}
 
 		const userData = await this.client.findOrCreateUser({ id: message.author.id });
@@ -24,13 +27,16 @@ module.exports = class extends Event {
 			if (afkReason) {
 				userData.afk = null;
 				await userData.save();
-				return message.reply(`**${message.author.username}**, your AFK status has just been deleted!`).then(msg => this.client.setTimeout(() => msg.delete(), 5000));
+				return message.reply(`You're no longer AFK!`).then(msg => this.client.setTimeout(() => msg.delete(), 5000));
 			}
 
 			message.mentions.users.forEach(async (user) => {
 				const dataUser = await this.client.findOrCreateUser({ id: user.id });
 				if (dataUser.afk) {
-					return message.reply(`**${user.tag}** is currently AFK!\nReason: ${dataUser.afk}`).then(msg => this.client.setTimeout(() => msg.delete(), 15000));
+					return message.reply([
+						`<@${user.id}> is currently AFK!`,
+						`***Reason:*** ${dataUser.afk}`
+					].join('\n')).then(msg => this.client.setTimeout(() => msg.delete(), 15000));
 				}
 			});
 		}
@@ -48,7 +54,7 @@ module.exports = class extends Event {
 				if (userPermCheck) {
 					const missing = message.channel.permissionsFor(message.member).missing(userPermCheck);
 					if (missing.length) {
-						return message.reply(`You're missing ${this.client.utils.formatArray(missing.map(this.client.utils.formatPerms))} permissions, you need them to use this command.`);
+						return message.reply(`You don't have ${this.client.utils.formatArray(missing.map(this.client.utils.formatPerms))} permission, you need it to continue this command!`);
 					}
 				}
 
@@ -56,21 +62,21 @@ module.exports = class extends Event {
 				if (clientPermCheck) {
 					const missing = message.channel.permissionsFor(message.guild.me).missing(clientPermCheck);
 					if (missing.length) {
-						return message.reply(`I'm missing ${this.client.utils.formatArray(missing.map(this.client.utils.formatPerms))} permissions, I need them to use this command.`);
+						return message.reply(`I don't have ${this.client.utils.formatArray(missing.map(this.client.utils.formatPerms))} permission, I need it to continue this command!`);
 					}
 				}
 
 				if (command.nsfw && !message.channel.nsfw) {
-					return message.reply('This command can only be ran in a NSFW marked channel.');
+					return message.reply('This command is only accessible on NSFW channels!');
 				}
 			}
 
 			if (command.disabled) {
-				return message.reply('This command is currently disabled!');
+				return message.reply('This command is currently inaccessible for some reason!');
 			}
 
 			if (command.ownerOnly && !this.client.utils.checkOwner(message.author.id)) {
-				return message.reply('Only developers can use this command.');
+				return message.reply('This command is only accessible for developers!');
 			}
 
 			if (!this.client.cooldowns.has(command.name)) {
@@ -86,7 +92,7 @@ module.exports = class extends Event {
 
 				if (now < expirationTime) {
 					const timeLeft = (expirationTime - now) / 1000;
-					return message.reply(`You must wait **${timeLeft.toFixed(2)}** second(s) to be able to run the \`${command.name}\` command again!`)
+					return message.reply(`You've to wait **${timeLeft.toFixed(2)}** second(s) before you can use this command again!`)
 						.then(msg => this.client.setTimeout(() => msg.delete(), expirationTime - now));
 				}
 			}
@@ -100,7 +106,7 @@ module.exports = class extends Event {
 				}
 			} catch (err) {
 				console.log(err);
-				return message.reply('Something went wrong... Please retry again later!');
+				return message.reply('Something went wrong, please contact the developer to fix it!');
 			}
 		}
 	}
