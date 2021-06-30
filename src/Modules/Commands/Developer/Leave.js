3d6e799a4db0bd66f6a3277ed51100dc06a02b57
Rygent <marizky65@gmail.com>
2021-07-01 06:26:50 +0700

    Developer: Added "Leave" command
    Signed-off-by: Rygent <marizky65@gmail.com>

---
+++ src/Modules/Commands/Developer/Leave.js
@@ -0,0 +1,60 @@
+const Command = require('../../../Structures/Command.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Leave a guild.',
+			category: 'Developer',
+			ownerOnly: true
+		});
+	}
+
+	/* eslint-disable consistent-return */
+	async run(message, [id]) {
+		if (!id) {
+			return message.reply({ content: 'Please provide a guild ID!' });
+		}
+
+		const guild = this.client.guilds.cache.get(id);
+		if (!guild) {
+			return message.reply({ content: 'Invalid guild ID!' });
+		}
+
+		const row = new MessageActionRow()
+			.addComponents(new MessageButton()
+				.setStyle('SUCCESS')
+				.setLabel('Confirm')
+				.setCustomID('confirm'))
+			.addComponents(new MessageButton()
+				.setStyle('DANGER')
+				.setLabel('Cancel')
+				.setCustomID('cancel'));
+
+		return message.reply({ content: `Please confirm if you want to leave from the **${guild.name}** guild!`, components: [row] }).then(msg => {
+			const filter = (button) => button.user.id === message.author.id;
+			const collector = msg.createMessageComponentInteractionCollector(filter, { time: 10000 });
+
+			collector.on('collect', async (button) => {
+				if (button.customID === 'confirm') {
+					await guild.leave();
+					return button.update({ content: `Successfully left guild **${guild.name}**!`, components: [] }).then(this.client.setTimeout(() => message.delete() && msg.delete(), 10000));
+				}
+
+				if (button.customID === 'cancel') {
+					collector.stop();
+					return button.update({ content: 'Command has been cancelled!', components: [] }).then(this.client.setTimeout(() => message.delete() && msg.delete(), 10000));
+				}
+			});
+
+			collector.on('end', (collected) => {
+				if (collected.size === 0) {
+					return msg.edit({ content: 'Time has run out, please use the command again!', components: [] }).then(this.client.setTimeout(() => message.delete() && msg.delete(), 5000));
+				}
+			});
+		});
+	}
+
+};
