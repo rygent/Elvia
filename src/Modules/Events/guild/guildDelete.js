4a0e59d9b71fe04a09dbf8fee6286e533424f671
Rygent <marizky65@gmail.com>
2021-07-01 05:52:15 +0700

    guildDelete: Added a webhook for sending logs
    Signed-off-by: Rygent <marizky65@gmail.com>

--- src/Modules/Events/guild/guildDelete.js
+++ src/Modules/Events/guild/guildDelete.js
@@ -1,24 +1,25 @@
 const Event = require('../../../Structures/Event.js');
-const { MessageEmbed } = require('discord.js');
-const { Colors, Supports } = require('../../../Structures/Configuration.js');
+const { MessageEmbed, WebhookClient } = require('discord.js');
+const { Access, Colors } = require('../../../Structures/Configuration.js');
+const webhook = new WebhookClient(Access.WEBHOOK_ID, Access.WEBHOOK_TOKEN);
 
 module.exports = class extends Event {
 
 	async run(guild) {
 		if (!guild.available) return;
+		const guildOwner = await guild.fetchOwner();
 
 		const embed = new MessageEmbed()
 			.setColor(Colors.RED)
-			.setTitle(`${this.client.user.username} left a server.`)
-			.setThumbnail(guild.iconURL({ dynamic: true, size: 512 }))
+			.setTitle(`${this.client.user.username} left a Server!`)
+			.setThumbnail(guild.iconURL() ? guild.iconURL({ dynamic: true, size: 512 }) : `https://guild-default-icon.herokuapp.com/${encodeURIComponent(guild.nameAcronym)}`)
 			.setDescription([
-				`***Server:*** ${guild.name}`,
-				`***ID:*** \`${guild.id}\``,
-				`***Owner:*** <@${guild.ownerID}>`
-			].join('\n'));
+				`***Server:*** ${guild.name} (\`${guild.id}\`)`,
+				`***Owner:*** ${guildOwner.user.tag} (\`${guildOwner.id}\`)`
+			].join('\n')) // eslint-disable-next-line max-len
+			.setFooter(`${this.client.guilds.cache.size.formatNumber()} guilds | ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).formatNumber()} users`, this.client.user.avatarURL({ dynamic: true }));
 
-		const sendChannel = this.client.channels.cache.get(Supports.GUILD_LOGS);
-		sendChannel.send({ embeds: [embed] });
+		webhook.send({ username: this.client.user.username, avatarURL: this.client.user.displayAvatarURL({ dynamic: true }), embeds: [embed] });
 	}
 
 };
