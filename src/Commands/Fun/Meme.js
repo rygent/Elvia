d3e0eac7dcafa6f35a1589b1fc83bbde7619af33
Rygent <marizky65@gmail.com>
2021-07-21 12:06:26 +0700

    feat: added meme command

---
+++ src/Commands/Fun/Meme.js
@@ -0,0 +1,31 @@
+const Command = require('../../Structures/Command.js');
+const { MessageEmbed } = require('discord.js');
+const { Color } = require('../../Utils/Configuration.js');
+const axios = require('axios');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: [],
+			description: 'Returns a meme.',
+			category: 'Fun',
+			cooldown: 3000
+		});
+	}
+
+	async run(message) {
+		const headers = { 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36' };
+		const data = await axios.get('https://meme-api.herokuapp.com/gimme', { headers }).then(res => res.data);
+
+		const embed = new MessageEmbed()
+			.setColor(Color.DEFAULT)
+			.setTitle(data.title)
+			.setDescription(`[Click here if the image failed to load.](${data.url})`)
+			.setImage(data.url)
+			.setFooter(`Responded in ${this.client.utils.responseTime(message)}`, message.author.avatarURL({ dynamic: true }));
+
+		return message.reply({ embeds: [embed] });
+	}
+
+};
