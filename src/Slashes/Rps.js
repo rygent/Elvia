986fa3e392997393cccb3bd32145e9945ac709fb
Rygent <marizky65@gmail.com>
2021-07-21 13:38:22 +0700

    feat: added rps slash command

---
+++ src/Slashes/Rps.js
@@ -0,0 +1,49 @@
+const Slash = require('../Structures/Slash.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = class extends Slash {
+
+	constructor(...args) {
+		super(...args, {
+			description: 'Play Rock Paper Scissors with bot'
+		});
+	}
+
+	async run(interaction) {
+		const button = new MessageActionRow()
+			.addComponents(new MessageButton()
+				.setStyle('SECONDARY')
+				.setLabel('Rock')
+				.setEmoji('🪨')
+				.setCustomId('rock'))
+			.addComponents(new MessageButton()
+				.setStyle('SECONDARY')
+				.setLabel('Paper')
+				.setEmoji('📄')
+				.setCustomId('paper'))
+			.addComponents(new MessageButton()
+				.setStyle('SECONDARY')
+				.setLabel('Scissors')
+				.setEmoji('✂️')
+				.setCustomId('scissors'));
+
+		const choices = ['rock', 'paper', 'scissors'];
+		const result = choices[Math.floor(Math.random() * 3)];
+
+		await interaction.reply({ content: 'Choose one of the buttons below to start the game!', components: [button], ephemeral: true });
+
+		const filter = (button) => button.user.id === interaction.user.id;
+		interaction.channel.awaitMessageComponent({ filter, time: 15000 }).then((button) => {
+			const winChoice = (button.customId === 'rock' && result === 'scissors') || (button.customId === 'paper' && result === 'rock') || (button.customId === 'scissors' && result === 'paper');
+
+			if (winChoice) {
+				return button.update({ content: `You won, you choose \`${button.customId.toProperCase()}\` while I choose \`${result.toProperCase()}\`!`, components: [] });
+			} else if (button.customId === result) {
+				return button.update({ content: `We tied, you choose \`${button.customId.toProperCase()}\` while I choose \`${result.toProperCase()}\`!`, components: [] });
+			} else {
+				return button.update({ content: `You lost, you choose \`${button.customId.toProperCase()}\` while I choose \`${result.toProperCase()}\`!`, components: [] });
+			}
+		}).catch(() => interaction.editReply({ content: 'Time\'s up! Please send the command again!', components: [] }));
+	}
+
+};
