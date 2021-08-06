d210c9d3e92bddfe29ffb8e8090cc6b21336997b
Rygent <marizky65@gmail.com>
2021-08-06 17:07:49 +0700

    fix(Invite): fix invalid scope

--- src/Slashes/Invite.js
+++ src/Slashes/Invite.js
@@ -15,7 +15,7 @@ module.exports = class extends Slash {
 			.addComponents(new MessageButton()
 				.setStyle('LINK')
 				.setLabel('Click here to invite!')
-				.setURL(`https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=${Access.INVITE_PERMISSION}&scope=${Access.INVITE_SCOPE}`));
+				.setURL(`https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=${Access.INVITE_PERMISSION}&scope=${Access.INVITE_SCOPE.join('%20')}`));
 
 		return interaction.reply({ content: 'Invite me to your server!', components: [button], ephemeral: true });
 	}
