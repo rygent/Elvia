ac54d5c39e6c63759daf45ddeafcb433d9c8833a
Rygent <marizky65@gmail.com>
2021-09-25 06:03:35 +0700

    refactor(Logger): add time and change console to stdout

--- src/Modules/Logger.js
+++ src/Modules/Logger.js
@@ -1,26 +1,40 @@
+const { inspect } = require('util');
 const chalk = require('chalk');
+const moment = require('moment');
 
 module.exports = class Logger {
 
 	static log({ content, type = 'log' }) {
 		switch (type) {
 			case 'log': {
-				return console.log(`[${chalk.blueBright('INFO')}] ${content}`);
+				return this.console({ content, status: 'INFO', color: 'blueBright' });
 			}
 			case 'warn': {
-				return console.log(`[${chalk.yellowBright('WARN')}] ${content}`);
+				return this.console({ content, status: 'WARN', color: 'yellowBright' });
 			}
 			case 'error': {
-				return console.log(`[${chalk.redBright('ERROR')}] ${content}`);
+				return this.console({ content, status: 'ERROR', color: 'redBright', error: true });
 			}
 			case 'debug': {
-				return console.log(`[${chalk.grey('DEBUG')}] ${content}`);
+				return this.console({ content, status: 'DEBUG', color: 'grey' });
 			}
 			case 'ready': {
-				return console.log(`[${chalk.greenBright('BOOT')}] ${content}`);
+				return this.console({ content, status: 'BOOT', color: 'greenBright' });
 			}
 			default: throw new TypeError('Logger type must be either warn, debug, log, ready or error.');
 		}
+	}
+
+	static console({ content, status, color, error = false }) {
+		const timestamp = chalk.dim(moment().utc().format('DD-MM-YYYY HH:mm:ss z'));
+		const stream = error ? process.stderr : process.stdout;
+		stream.write(`${timestamp} | [${chalk[color].bold(status)}] ${this.clean(content)}\n`);
+	}
+
+	static clean(content) {
+		if (typeof content === 'string') return content;
+		const cleaned = inspect(content, { depth: Infinity });
+		return cleaned;
 	}
 
 };
