70a65abd8c01be106b99690b50302ac871ca93bc
Rygent <marizky65@gmail.com>
2022-01-18 16:55:47 +0700

    refactor(Kick): rework and remove unnecessary code

--- src/Commands/Interaction/Slash/Moderation/Kick.js
+++ src/Commands/Interaction/Slash/Moderation/Kick.js
@@ -1,6 +1,7 @@
 const Interaction = require('../../../../Structures/Interaction.js');
-const { MessageEmbed } = require('discord.js');
+const { Formatters, MessageEmbed } = require('discord.js');
 const { Color } = require('../../../../Utils/Configuration.js');
+const { uid } = require('uid');
 
 module.exports = class extends Interaction {
 
@@ -15,64 +16,46 @@ module.exports = class extends Interaction {
 
 	async run(interaction) {
 		const member = await interaction.options.getMember('member', true);
-		const reason = await interaction.options.getString('reason') || 'No reason provided';
+		const reason = await interaction.options.getString('reason');
 
 		const guildData = await this.client.findOrCreateGuild({ id: interaction.guildId });
-		const memberData = await this.client.findOrCreateMember({ id: member.id, guildId: interaction.guildId });
-
-		await interaction.deferReply({ ephemeral: true });
 
 		if (!member.kickable) return interaction.editReply({ content: `I can't kicked **${member.displayName}**! For having a higher role than mine!` });
 		if (!member.manageable) {
 			return interaction.editReply({ content: 'You can\'t kick a member who has an higher or equal role hierarchy to yours!' });
 		}
 
-		return interaction.guild.members.kick(member, { reason }).then(async () => {
-			if (!member.user.bot) {
-				return member.send({ content: [
-					`Hello **${member.user.username}**, You've just been kicked from _${interaction.guild.name}_ by _${interaction.user.tag}_!`,
-					`***Reason:*** ${reason}`
-				].join('\n') });
-			}
+		await member.kick(`${reason ? `${reason} (Kicked by ${interaction.user.tag})` : `(Kicked by ${interaction.user.tag})`}`);
 
-			interaction.editReply({ content: [
-				`**${member.displayName}** has been successfully kicked from this guild!`,
-				`***Reason:*** ${reason}`
-			].join('\n') });
+		const uniqueId = uid(24);
 
-			const caseInfo = {
-				channel: interaction.channel.id,
-				moderator: interaction.user.id,
-				date: Date.now(),
-				type: 'kick',
-				case: guildData.casesCount,
-				reason
-			};
+		guildData.casesCount++;
+		await guildData.save();
 
-			memberData.sanctions.push(caseInfo);
-			await memberData.save();
+		interaction.reply({ content: [
+			`**${member.user.tag}** was kicked!`,
+			`${reason ? `\n>>> ***Reason:*** ${reason}` : ''}`
+		].join('') });
 
-			guildData.casesCount++;
-			await guildData.save();
+		if (guildData.plugins.moderations) {
+			const channel = interaction.guild.channels.cache.get(guildData.plugins.moderations);
+			if (!channel) return;
 
-			if (guildData.plugins.moderations) {
-				const sendChannel = interaction.guild.channels.cache.get(guildData.plugins.moderations);
-				if (!sendChannel) return;
+			const embed = new MessageEmbed()
+				.setColor(Color.ORANGE)
+				.setAuthor({ name: `Actioned by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+				.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+				.setDescription([
+					`***ID:*** \`${uniqueId}\``,
+					`***User:*** ${member.user.tag} (\`${member.user.id}\`)`,
+					`***Action:*** Kick`,
+					`***Reason:*** ${reason || 'None specified'}`,
+					`***Date:*** ${Formatters.time(new Date(Date.now()))}`
+				].join('\n'))
+				.setFooter({ text: `Powered by ${this.client.user.username}  •  Case #${guildData.casesCount}`, iconURL: this.client.user.avatarURL({ dynamic: true }) });
 
-				const embed = new MessageEmbed()
-					.setColor(Color.RED)
-					.setAuthor({ name: `Actioned by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-					.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-					.setDescription([
-						`***Member:*** ${member.user.tag} (\`${member.user.id}\`)`,
-						`***Action:*** Kick`,
-						`***Reason:*** ${reason}`
-					].join('\n'))
-					.setFooter({ text: `Powered by ${this.client.user.username}  •  Case #${guildData.casesCount}`, iconURL: this.client.user.avatarURL({ dynamic: true }) });
-
-				return sendChannel.send({ embeds: [embed] });
-			}
-		});
+			return channel.send({ embeds: [embed] });
+		}
 	}
 
 };
