03692ccbbdafcd5ef07e0f56036e412515a2093e
Rygent <marizky65@gmail.com>
2022-01-19 12:50:13 +0700

    feat(Avatar): add options to get user's guild avatar

--- src/Commands/Interaction/Slash/Miscellaneous/Avatar.js
+++ src/Commands/Interaction/Slash/Miscellaneous/Avatar.js
@@ -13,21 +13,40 @@ module.exports = class extends Interaction {
 
 	async run(interaction) {
 		const user = await interaction.options.getUser('user') || interaction.user;
+		const guild = await interaction.options.getBoolean('guild');
 
-		const button = new MessageActionRow()
-			.addComponents(new MessageButton()
-				.setStyle('LINK')
-				.setLabel('Open in Browser')
-				.setURL(user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 })));
+		const button = new MessageButton()
+			.setStyle('LINK')
+			.setLabel('Open in Browser');
 
 		const embed = new MessageEmbed()
 			.setColor(Color.DEFAULT)
-			.setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true }) })
-			.setDescription(`\`ID: ${user.id}\``)
-			.setImage(user.displayAvatarURL({ dynamic: true, size: 512 }))
 			.setFooter({ text: `Powered by ${this.client.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
 
-		return interaction.reply({ embeds: [embed], components: [button] });
+		if (guild) {
+			try {
+				const member = await interaction.guild.members.fetch(user);
+				if (!member.avatar) return interaction.reply({ content: `**${member.user.tag}**'s has no server avatar!`, ephemeral: true });
+
+				button.setURL(member.avatarURL({ format: 'png', dynamic: true, size: 4096 }));
+
+				embed.setAuthor({ name: member.user.tag, iconURL: member.avatarURL({ dynamic: true }) });
+				embed.setDescription(`***ID:*** \`${member.user.id}\``);
+				embed.setImage(member.avatarURL({ dynamic: true, size: 512 }));
+
+				return interaction.reply({ embeds: [embed], components: [new MessageActionRow().addComponents([button])] });
+			} catch {
+				return interaction.reply({ content: 'This user is not on this server!', ephemeral: true });
+			}
+		}
+
+		button.setURL(user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }));
+
+		embed.setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true }) });
+		embed.setDescription(`***ID:*** \`${user.id}\``);
+		embed.setImage(user.displayAvatarURL({ dynamic: true, size: 512 }));
+
+		return interaction.reply({ embeds: [embed], components: [new MessageActionRow().addComponents([button])] });
 	}
 
 };
