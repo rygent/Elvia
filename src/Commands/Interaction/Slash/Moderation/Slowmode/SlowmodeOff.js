18e882f40aa185fe2be4df6c2103dff7fbc21d87
Rygent <marizky65@gmail.com>
2022-01-24 06:53:56 +0700

    feat: add slowmode off command

---
+++ src/Commands/Interaction/Slash/Moderation/Slowmode/SlowmodeOff.js
@@ -0,0 +1,23 @@
+const Interaction = require('../../../../../Structures/Interaction.js');
+
+module.exports = class extends Interaction {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'slowmode',
+			subCommand: 'off',
+			description: 'Turn off slowmode.',
+			memberPerms: ['MANAGE_CHANNELS'],
+			clientPerms: ['MANAGE_CHANNELS']
+		});
+	}
+
+	async run(interaction) {
+		const channel = await interaction.options.getChannel('channel') || interaction.channel;
+
+		await channel.setRateLimitPerUser(0);
+
+		return interaction.reply({ content: 'Channel slowmode was disabled.' });
+	}
+
+};
