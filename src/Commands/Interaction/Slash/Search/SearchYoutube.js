85211bbd4d7ecd5ab37ad82a1be82331642d1266
Rygent <marizky65@gmail.com>
2022-01-23 19:43:39 +0700

    fix(SearchYoutube): `DiscordAPIError: Unknown interaction`

--- src/Commands/Interaction/Slash/Search/SearchYoutube.js
+++ src/Commands/Interaction/Slash/Search/SearchYoutube.js
@@ -16,7 +16,7 @@ module.exports = class extends Interaction {
 
 	async run(interaction) {
 		const search = await interaction.options.getString('search', true);
-		await interaction.deferReply();
+		await interaction.deferReply({ fetchReply: true });
 
 		const data = await api.searchVideos(search, 25);
 		if (data.length === 0) return interaction.editReply({ content: 'Nothing found for this search.' });
@@ -31,40 +31,42 @@ module.exports = class extends Interaction {
 					value: res.id
 				}))));
 
-		await interaction.editReply({ content: `I found **${data.length}** possible matches, please select one of the following:`, components: [select] });
+		return interaction.editReply({ content: `I found **${data.length}** possible matches, please select one of the following:`, components: [select] }).then(message => {
+			const collector = message.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: 60_000 });
 
-		const collector = interaction.channel.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: 60_000 });
+			collector.on('collect', async (i) => {
+				if (i.user.id !== interaction.user.id) return i.deferUpdate();
+				await i.deferUpdate();
 
-		collector.on('collect', async (i) => {
-			if (i.user.id !== interaction.user.id) return i.deferUpdate();
-			await i.deferUpdate();
+				const [choices] = i.values;
+				const result = data.find(x => x.id === choices);
 
-			const [choices] = i.values;
-			const result = data.find(x => x.id === choices);
+				const button = new MessageActionRow()
+					.addComponents(new MessageButton()
+						.setStyle('LINK')
+						.setLabel('Open in Browser')
+						.setURL(result.shortURL));
 
-			const button = new MessageActionRow()
-				.addComponents(new MessageButton()
-					.setStyle('LINK')
-					.setLabel('Open in Browser')
-					.setURL(result.shortURL));
+				const embed = new MessageEmbed()
+					.setColor(Color.DEFAULT)
+					.setAuthor({ name: 'YouTube', iconURL: 'https://i.imgur.com/lbS6Vil.png', url: 'https://youtube.com/' })
+					.setTitle(result.title)
+					.setDescription([
+						`**${result.channel.title}**`,
+						`${result.description}\n`,
+						`***Published:*** ${Formatters.time(new Date(result.publishedAt))}`
+					].join('\n'))
+					.setImage(result.thumbnails.high.url)
+					.setFooter({ text: 'Powered by YouTube', iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
 
-			const embed = new MessageEmbed()
-				.setColor(Color.DEFAULT)
-				.setAuthor({ name: 'YouTube', iconURL: 'https://i.imgur.com/lbS6Vil.png', url: 'https://youtube.com/' })
-				.setTitle(result.title)
-				.setDescription([
-					`**${result.channel.title}**`,
-					`${result.description}\n`,
-					`***Published:*** ${Formatters.time(new Date(result.publishedAt))}`
-				].join('\n'))
-				.setImage(result.thumbnails.high.url)
-				.setFooter({ text: 'Powered by YouTube', iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
+				return i.editReply({ content: '\u200B', embeds: [embed], components: [button] });
+			});
 
-			return i.editReply({ content: '\u200B', embeds: [embed], components: [button] });
-		});
-
-		collector.on('end', (collected, reason) => {
-			if (reason === 'time') return interaction.deleteReply();
+			collector.on('end', (collected, reason) => {
+				if ((collected.size === 0 || collected.filter(x => x.user.id === interaction.user.id).size === 0) && reason === 'time') {
+					return interaction.deleteReply();
+				}
+			});
 		});
 	}
 
