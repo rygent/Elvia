61519b69efb381333615a7356b7b091c5d82d5d5
Rygent <marizky65@gmail.com>
2022-01-24 16:55:52 +0700

    feat(Translate): add default source language to auto and destination language to server language

--- src/Commands/Interaction/Slash/Miscellaneous/Translate.js
+++ src/Commands/Interaction/Slash/Miscellaneous/Translate.js
@@ -8,31 +8,42 @@ module.exports = class extends Interaction {
 	constructor(...args) {
 		super(...args, {
 			name: 'translate',
-			description: 'Translate text.'
+			description: 'Translate your text.'
 		});
 	}
 
 	async run(interaction) {
-		const fromLanguage = await interaction.options.getString('from', true);
-		const toLanguage = await interaction.options.getString('to', true);
 		const text = await interaction.options.getString('text', true);
+		const fromLanguage = await interaction.options.getString('from');
+		const toLanguage = await interaction.options.getString('to');
+
+		let locale;
+		if (interaction.guildLocale === 'zh-CN' || interaction.guildLocale === 'zh-TW') {
+			locale = interaction.guildLocale;
+		} else {
+			locale = new Intl.Locale(interaction.guildLocale).language;
+		}
+
+		const target = toLanguage || locale;
 
 		try {
-			const translated = await translate(text, { from: fromLanguage.trim(), to: toLanguage.trim() });
+			const translated = await translate(text, { from: fromLanguage || 'auto', to: target });
 			const from = translated.from.language.iso;
 
 			const embed = new MessageEmbed()
-				.setColor(Color.G_TRANSLATE)
+				.setColor(Color.DEFAULT)
 				.setAuthor({ name: 'Google Translate', iconURL: 'https://i.imgur.com/1JS81kv.png', url: 'https://translate.google.com/' })
 				.setDescription([
 					`${translated.text}\n`,
-					`Translation from ***${this.client.utils.formatLanguage(from)}*** to ***${this.client.utils.formatLanguage(toLanguage.trim())}***`
+					`Translation from ***${this.client.utils.formatLanguage(from)}*** to ***${this.client.utils.formatLanguage(target)}***`
 				].join('\n'))
 				.setFooter({ text: 'Powered by Google Translate', iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
 
 			return interaction.reply({ embeds: [embed] });
-		} catch {
-			return interaction.reply({ content: 'Please send valid **[ISO 639-1](<https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes>)** languages codes.', ephemeral: true });
+		} catch (error) {
+			if (error.code === 400) {
+				return interaction.reply({ content: 'Please send valid **[ISO 639-1](<https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes>)** languages codes.', ephemeral: true });
+			}
 		}
 	}
 
