aec56df6470c2a1895497fb9bafe06876b875643
Rygent <marizky65@gmail.com>
2022-01-24 07:39:37 +0700

    feat: add command of emoji add

---
+++ src/Commands/Interaction/Slash/Moderation/Emoji/EmojiAdd.js
@@ -0,0 +1,39 @@
+const Interaction = require('../../../../../Structures/Interaction.js');
+const { Util } = require('discord.js');
+
+module.exports = class extends Interaction {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'emoji',
+			subCommand: 'add',
+			description: 'Add an emoji to the server.',
+			memberPerms: ['MANAGE_EMOJIS_AND_STICKERS'],
+			clientPerms: ['MANAGE_EMOJIS_AND_STICKERS']
+		});
+	}
+
+	async run(interaction) {
+		const name = await interaction.options.getString('name', true);
+		const emoji = await interaction.options.getString('emoji', true);
+
+		const regex = RegExp(/(https?:\/\/[^\s]+)/g);
+
+		const parseEmoji = Util.parseEmoji(emoji);
+
+		try {
+			let emojis;
+			if (emoji.match(regex)) {
+				emojis = await interaction.guild.emojis.create(emoji, name);
+			} else if (parseEmoji.id) {
+				const link = `https://cdn.discordapp.com/emojis/${parseEmoji.id}.${parseEmoji.animated ? 'gif' : 'png'}`;
+				emojis = await interaction.guild.emojis.create(link, name);
+			}
+
+			return interaction.reply({ content: `Emoji \`:${emojis.name}:\` ${emojis} was successfully added.` });
+		} catch {
+			return interaction.reply({ content: 'The emoji are invalid or you don\'t have more space on your server!', ephemeral: true });
+		}
+	}
+
+};
