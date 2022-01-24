fd59e7b75022d03474cbd9541e30fa2c085b064e
Rygent <marizky65@gmail.com>
2022-01-24 07:38:15 +0700

    feat: add emoji list command

---
+++ src/Commands/Interaction/Slash/Miscellaneous/Emoji/EmojiList.js
@@ -0,0 +1,30 @@
+const Interaction = require('../../../../../Structures/Interaction.js');
+
+module.exports = class extends Interaction {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'emoji',
+			subCommand: 'list',
+			description: 'List server emojis.'
+		});
+	}
+
+	async run(interaction) {
+		const standardEmoji = [];
+		const animatedEmoji = [];
+
+		await interaction.guild.emojis.cache.forEach(x => {
+			if (x.animated) {
+				animatedEmoji.push(x.toString());
+			} else {
+				standardEmoji.push(x.toString());
+			}
+		});
+
+		if (animatedEmoji.length === 0 && standardEmoji.length === 0) return interaction.reply({ content: 'There are no emojis in this server.', ephemeral: true });
+
+		return interaction.reply({ content: `${standardEmoji.join(' ')} ${animatedEmoji.join(' ')}`, ephemeral: true });
+	}
+
+};
