958fee218cf7e753ff0cde7ee0d249b9d4145805
Rygent <marizky65@gmail.com>
2022-01-24 18:55:30 +0700

    feat: add purge starts-with command

---
+++ src/Commands/Interaction/Slash/Moderation/Purge/PurgeStartsWith.js
@@ -0,0 +1,40 @@
+const Interaction = require('../../../../../Structures/Interaction.js');
+
+module.exports = class extends Interaction {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'purge',
+			subCommand: 'starts-with',
+			description: 'Purge messages that starts with specified content in the channel.',
+			memberPerms: ['MANAGE_MESSAGES'],
+			clientPerms: ['MANAGE_MESSAGES']
+		});
+	}
+
+	async run(interaction) {
+		const content = await interaction.options.getString('content', true);
+		const amount = await interaction.options.getInteger('amount');
+
+		const fetch = await interaction.channel.messages.fetch({ limit: amount });
+		const data = [];
+
+		fetch.map(m => m).forEach(message => {
+			if (message.content.startsWith(content) && !message.pinned) return data.push(message);
+		});
+
+		try {
+			return interaction.channel.bulkDelete(data.length ? data : 1, true).then(async (message) => {
+				if (!amount) {
+					await interaction.reply({ content: `Successfully deleted **${message.size}** messages.` });
+				} else {
+					await interaction.reply({ content: `Successfully deleted **${message.size}**/**${amount}** messages.` });
+				}
+				setTimeout(() => interaction.deleteReply(), 10_000);
+			});
+		} catch {
+			return interaction.reply({ content: 'You can only delete the messages which are not older than 14 days.', ephemeral: true });
+		}
+	}
+
+};
