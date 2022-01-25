569ae4195be1ccb01e13f99f36aff81a32d7de56
Rygent <marizky65@gmail.com>
2022-01-25 13:47:57 +0700

    feat: add interact greet command

---
+++ src/Commands/Interaction/Slash/Interact/InteractGreet.js
@@ -0,0 +1,31 @@
+const Interaction = require('../../../../Structures/Interaction.js');
+const { MessageEmbed } = require('discord.js');
+const { Color } = require('../../../../Utils/Configuration.js');
+const axios = require('axios');
+
+module.exports = class extends Interaction {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'interact',
+			subCommand: 'greet',
+			description: 'Greet someone.'
+		});
+	}
+
+	async run(interaction) {
+		const member = await interaction.options.getMember('user', true);
+
+		const headers = { 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36' };
+		const result = await axios.get(`https://api.waifu.pics/sfw/wave`, { headers }).then(res => res.data);
+
+		const embed = new MessageEmbed()
+			.setColor(Color.DEFAULT)
+			.setDescription(`**${interaction.user.username}** greets **${member.user.username}**.`)
+			.setImage(result.url)
+			.setFooter({ text: `Powered by ${this.client.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
+
+		return interaction.reply({ embeds: [embed] });
+	}
+
+};
