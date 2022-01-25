b6b703021234c12139494217b8ed1b6dc84a0dbf
Rygent <marizky65@gmail.com>
2022-01-25 09:14:21 +0700

    feat: add purge images command

---
+++ src/Commands/Interaction/Slash/Moderation/Purge/PurgeImages.js
@@ -0,0 +1,41 @@
+const Interaction = require('../../../../../Structures/Interaction.js');
+
+module.exports = class extends Interaction {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'purge',
+			subCommand: 'images',
+			description: 'Purge messages that contain images in the channel.',
+			memberPerms: ['MANAGE_MESSAGES'],
+			clientPerms: ['MANAGE_MESSAGES']
+		});
+	}
+
+	async run(interaction) {
+		const amount = await interaction.options.getInteger('amount');
+		const regex = RegExp(/image\/(avif|gif|jpeg|png|svg\+xml)/g);
+
+		const fetch = await interaction.channel.messages.fetch({ limit: amount });
+		const data = [];
+
+		fetch.map(m => m).forEach(message => {
+			const contentType = message.attachments.map(m => m.contentType);
+			if (regex.test(contentType) && !message.pinned) return data.push(message);
+		});
+
+		try {
+			return interaction.channel.bulkDelete(data.length ? data : 1, true).then(async (message) => {
+				if (!amount) {
+					await interaction.reply({ content: `Successfully deleted **${message.size}** messages.` });
+				} else {
+					await interaction.reply({ content: `Successfully deleted **${message.size}**/**${amount}** messages.` });
+				}
+				setTimeout(() => interaction.deleteReply(), 10_000);
+			});
+		} catch {
+			return interaction.reply({ content: 'You can only delete the messages which are not older than 14 days.', ephemeral: true });
+		}
+	}
+
+};
