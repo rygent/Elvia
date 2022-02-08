d4406c8a7bdccaccc3096a8b9fa4c135299f808e
Rygent <marizky65@gmail.com>
2022-02-08 08:19:35 +0700

    refactor: export interaction data to .js file and separate it

--- src/Commands/Message/Developer/Deploy.js
+++ src/Commands/Message/Developer/Deploy.js
@@ -1,34 +1,36 @@
 const Command = require('../../../Structures/Command.js');
+const ChatInput = require('../../../Settings/ChatInputInteraction.js');
+const ContextMenu = require('../../../Settings/ContextMenuInteraction.js');
 
 module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			aliases: [''],
+			aliases: [],
 			description: 'Deploy the Slash Commands of this bot.',
 			category: 'Developer',
-			usage: '(guild_id)',
+			usage: '(guildId)',
 			ownerOnly: true
 		});
 	}
 
 	async run(message, [guildId]) {
-		const data = require('../../../../assets/json/InteractionData.json');
+		const InteractionData = [...ChatInput, ...ContextMenu];
 
 		if (guildId) {
 			const guild = await this.client.guilds.cache.get(guildId);
 			return message.reply({ content: `Attempting to set the Guild Slash Commands in \`${guild.name}\`...` }).then(async (m) => {
-				await guild?.commands.set(data).then((slashData) => { // eslint-disable-next-line id-length
-					m.edit({ content: `\`${slashData.size.formatNumber()}\` Commands (\`${slashData.map(d => d.options).flat().length}\` Subcommands) loaded for **${guild.name}**` });
-				}).catch((error) => {
+				await guild?.commands.set(InteractionData).then(data => {
+					m.edit({ content: `\`${data.size.formatNumber()}\` Commands (\`${data.map(i => i.options).flat().length.formatNumber()}\` Subcommands) loaded for **${guild.name}**` });
+				}).catch(error => {
 					this.client.logger.log({ content: error.stack, type: 'error' });
 					m.edit({ content: `Could not load the Slash Commands for ${guild.name}` });
 				});
 			});
 		} else {
 			return message.reply({ content: `Attempting to set the Global Slash Commands in \`${this.client.guilds.cache.size.formatNumber()} guilds\`...` }).then(async (m) => {
-				await this.client.application?.commands.set(data).then((slashData) => { // eslint-disable-next-line id-length
-					m.edit({ content: `\`${slashData.size.formatNumber()}\` Commands (\`${slashData.map(d => d.options).flat().length}\` Subcommands) loaded for all possible Guilds` });
+				await this.client.application?.commands.set(InteractionData).then(data => {
+					m.edit({ content: `\`${data.size.formatNumber()}\` Commands (\`${data.map(i => i.options).flat().length.formatNumber()}\` Subcommands) loaded for all possible Guilds` });
 				}).catch(() => {});
 			});
 		}
---
+++ src/Settings/ContextMenuInteraction.js
@@ -0,0 +1,6 @@
+module.exports = [
+	{
+		name: 'Translate Message',
+		type: 'MESSAGE'
+	}
+];
