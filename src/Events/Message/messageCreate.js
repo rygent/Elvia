00c73a8902926601c5530d76473a8ed0edd37c17
Rygent <marizky65@gmail.com>
2022-02-10 01:11:40 +0700

    feat(messageCreate): bring back permission check

--- src/Events/Message/messageCreate.js
+++ src/Events/Message/messageCreate.js
@@ -5,8 +5,14 @@ const { Access } = require('../../Settings/Configuration.js');
 module.exports = class extends Event {
 
 	async run(message) {
+		if (!message.inGuild() || message.author.bot) return;
+
+		const mentionRegex = RegExp(`^<@!?${this.client.user.id}>$`);
 		const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}> `);
-		if (!message.guild || message.author.bot) return;
+
+		if (message.content.match(mentionRegex)) {
+			return message.reply({ content: `The prefix for this server is \`${this.client.prefix}\`.` });
+		}
 
 		const prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : this.client.prefix;
 
@@ -16,20 +22,33 @@ module.exports = class extends Event {
 
 		const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
 		if (command) {
-			if (command.disabled) {
-				await message.channel.sendTyping();
+			if (command.disabled && !this.client.utils.checkOwner(message.author.id)) {
 				return message.reply({ content: 'This command is currently inaccessible!' });
 			}
 
-			if (message.guild) {
+			if (message.inGuild()) {
+				const memberPermCheck = command.memberPermission ? this.client.defaultPermission.add(command.memberPermission) : this.client.defaultPermission;
+				if (memberPermCheck) {
+					const missing = message.channel.permissionsFor(message.member).missing(memberPermCheck);
+					if (missing.length) {
+						return message.reply({ content: `You lack the ${this.client.utils.formatArray(missing.map(perms => `***${this.client.utils.formatPermission(perms)}***`))} permission(s) to continue.` });
+					}
+				}
+
+				const clientPermCheck = command.clientPermission ? this.client.defaultPermission.add(command.clientPermission) : this.client.defaultPermission;
+				if (clientPermCheck) {
+					const missing = message.channel.permissionsFor(message.guild.me).missing(clientPermCheck);
+					if (missing.length) {
+						return message.reply({ content: `I lack the ${this.client.utils.formatArray(missing.map(perms => `***${this.client.utils.formatPermission(perms)}***`))} permission(s) to continue.` });
+					}
+				}
+
 				if (command.nsfw && !message.channel.nsfw) {
-					await message.channel.sendTyping();
 					return message.reply({ content: 'This command is only accessible on NSFW channels!' });
 				}
 			}
 
 			if (command.ownerOnly && !this.client.utils.checkOwner(message.author.id)) {
-				await message.channel.sendTyping();
 				return message.reply({ content: 'This command is only accessible for developers!' });
 			}
 
@@ -37,13 +56,14 @@ module.exports = class extends Event {
 				await message.channel.sendTyping();
 				await command.run(message, args);
 			} catch (error) {
+				this.client.logger.log({ content: error.stack, type: 'error' });
+
 				const button = new MessageActionRow()
 					.addComponents(new MessageButton()
 						.setStyle('LINK')
 						.setLabel('Support Server')
 						.setURL(`https://discord.gg/${Access.InviteCode}`));
 
-				this.client.logger.log({ content: error.stack, type: 'error' });
 				return message.reply({ content: 'Something went wrong, please report it to our **guild support**!', components: [button] });
 			}
 		}
--- src/Structures/Command.js
+++ src/Structures/Command.js
@@ -1,3 +1,5 @@
+const { Permissions } = require('discord.js');
+
 module.exports = class Command {
 
 	constructor(client, name, options = {}) {
@@ -7,6 +9,8 @@ module.exports = class Command {
 		this.description = options.description || 'No description provided.';
 		this.category = options.category || 'Miscellaneous';
 		this.usage = options.usage || '';
+		this.memberPermission = new Permissions(options.memberPermission).freeze();
+		this.clientPermission = new Permissions(options.clientPermission).freeze();
 		this.nsfw = options.nsfw || false;
 		this.ownerOnly = options.ownerOnly || false;
 		this.disabled = options.disabled || false;
