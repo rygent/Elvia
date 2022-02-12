d75dfd2d45263cb7b0955b646b072f3783557b76
Rygent <marizky65@gmail.com>
2022-02-12 09:02:03 +0700

    refactor(Kick): add ignored users and remove moderation log

--- src/Commands/Interaction/ChatInput/Moderation/Kick.js
+++ src/Commands/Interaction/ChatInput/Moderation/Kick.js
@@ -1,6 +1,4 @@
 const Interaction = require('../../../../Structures/Interaction.js');
-const { MessageEmbed } = require('discord.js');
-const { Color } = require('../../../../Settings/Configuration.js');
 
 module.exports = class extends Interaction {
 
@@ -14,43 +12,30 @@ module.exports = class extends Interaction {
 	}
 
 	async run(interaction) {
-		const member = await interaction.options.getMember('member', true);
+		const user = await interaction.options.getUser('member', true);
 		const reason = await interaction.options.getString('reason');
+
+		const member = await interaction.guild.members.cache.get(user.id);
+		if (!member) return interaction.reply({ content: 'Member not found, please verify that this user is a server member.', ephemeral: true });
+
+		if (member.user.id === interaction.user.id) return interaction.reply({ content: `You can't kick yourself.`, ephemeral: true });
+		if (member.user.id === this.client.user.id) return interaction.reply({ content: `You cannot kick me!`, ephemeral: true });
+		if (member.roles.highest.comparePositionTo(interaction.member.roles.highest) > 0) {
+			return interaction.reply({ content: 'You cannot kick a member who has a higher or equal role than yours.', ephemeral: true });
+		}
+		if (!member.kickable) return interaction.reply({ content: `I cannot kick a member who has a higher or equal role than mine.`, ephemeral: true });
 
 		const guildData = await this.client.findOrCreateGuild({ id: interaction.guildId });
 
-		if (!member.kickable) return interaction.reply({ content: `I can't kicked **${member.displayName}**! For having a higher role than mine!`, ephemeral: true });
-		if (!member.manageable) {
-			return interaction.reply({ content: 'You can\'t kick a member who has an higher or equal role hierarchy to yours!', ephemeral: true });
-		}
-
-		await member.kick(`${reason ? `${reason} (Kicked by ${interaction.user.tag})` : `(Kicked by ${interaction.user.tag})`}`);
+		await interaction.guild.members.kick(member, `${reason ? `${reason} (Kicked by ${interaction.user.tag})` : `(Kicked by ${interaction.user.tag})`}`);
 
 		guildData.casesCount++;
 		await guildData.save();
 
-		interaction.reply({ content: [
+		return interaction.reply({ content: [
 			`**${member.user.tag}** was kicked!`,
 			`${reason ? `\n***Reason:*** ${reason}` : ''}`
 		].join('') });
-
-		if (guildData.plugins.moderations) {
-			const channel = interaction.guild.channels.cache.get(guildData.plugins.moderations);
-			if (!channel) return;
-
-			const embed = new MessageEmbed()
-				.setColor(Color.DEFAULT)
-				.setAuthor({ name: `Actioned by ${interaction.user.tag}`, iconURL: interaction.user.avatarURL({ dynamic: true }) })
-				.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-				.setDescription([
-					`***User:*** ${member.user.tag} (\`${member.user.id}\`)`,
-					`***Action:*** Kick`,
-					`***Reason:*** ${reason || 'None specified'}`
-				].join('\n'))
-				.setFooter({ text: `Powered by ${this.client.user.username}  •  Case #${guildData.casesCount}`, iconURL: this.client.user.avatarURL({ dynamic: true }) });
-
-			return channel.send({ embeds: [embed] });
-		}
 	}
 
 };
