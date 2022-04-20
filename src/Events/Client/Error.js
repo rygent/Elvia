34bc89c3298d2ac3436e078045031759693b3ddd
Rygent <marizky65@gmail.com>
2022-04-20 07:26:20 +0700

    revert: refactor(Events): remove client error event
    This reverts commit c9630b1698f2663f76318d42d93a85809772448f.

---
+++ src/Events/Client/Error.js
@@ -0,0 +1,16 @@
+const Event = require('../../Structures/Event');
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'error',
+			once: false
+		});
+	}
+
+	async run(error) {
+		this.client.logger.error(error.stack);
+	}
+
+};
