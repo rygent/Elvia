8c342cb3ab39310858eef96e7b71d1e4b92a0404
Rygent <marizky65@gmail.com>
2022-09-20 10:07:48 +0700

    feat: add evaluate to interaction command

---
+++ src/Interactions/Slash/Developer/Evaluate.js
@@ -0,0 +1,24 @@
+import { ApplicationCommandType, ApplicationCommandOptionType } from 'discord-api-types/v10';
+
+export default {
+	name: 'evaluate',
+	description: 'Evaluates any JavaScript code.',
+	type: ApplicationCommandType.ChatInput,
+	options: [{
+		name: 'depth',
+		description: 'The inspection depth to apply.',
+		type: ApplicationCommandOptionType.Integer,
+		required: false
+	}, {
+		name: 'async',
+		description: 'Whether this code should be evaluated asynchronously.',
+		type: ApplicationCommandOptionType.Boolean,
+		required: false
+	}, {
+		name: 'ephemeral',
+		description: 'Whether the message should be visibled ephemerally.',
+		type: ApplicationCommandOptionType.Boolean,
+		required: false
+	}],
+	dm_permission: false
+};
