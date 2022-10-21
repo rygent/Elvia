4bac78d5769c747a686623864b3e4032c8447a68
Rygent <marizky65@gmail.com>
2022-10-21 16:37:58 +0700

    feat(interact): change api source and add new command

--- src/Commands/Slash/Social/Interact/Wink.js
+++ src/Commands/Slash/Social/Interact/Wink.js
@@ -7,20 +7,20 @@ export default class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			name: ['interact', 'baka'],
-			description: 'Say baka to someone.'
+			name: ['interact', 'wink'],
+			description: 'Wink someone.'
 		});
 	}
 
 	async run(interaction) {
 		const member = interaction.options.getMember('user');
 
-		const raw = await fetch(`https://nekos.best/api/v2/baka`, { method: 'GET' });
+		const raw = await fetch(`https://nekos.best/api/v2/wink`, { method: 'GET' });
 		const response = await raw.json().then(({ results }) => results[0]);
 
 		const embed = new EmbedBuilder()
 			.setColor(Colors.Default)
-			.setDescription(`${interaction.user.toString()} says that ${member.toString()} is a baka.`)
+			.setDescription(`${member.toString()}, you've got a wink from ${interaction.user.toString()}.`)
 			.setImage(response.url)
 			.setFooter({ text: `Powered by ${this.client.user.username}`, iconURL: interaction.user.avatarURL() });
 
