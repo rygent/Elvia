ed4fff016037afc1e26ed7346e8f54c532e90f48
Rygent <marizky65@gmail.com>
2022-10-25 10:06:21 +0700

    feat: implement prisma to some events

--- src/Events/Guild/GuildDelete.js
+++ src/Events/Guild/GuildDelete.js
@@ -1,7 +1,9 @@
 import Event from '../../Structures/Event.js';
 import { EmbedBuilder } from '@discordjs/builders';
 import { WebhookClient, parseWebhookURL } from 'discord.js';
+import { PrismaClient } from '@prisma/client';
 import { Colors, Links } from '../../Utils/Constants.js';
+const prisma = new PrismaClient();
 
 export default class extends Event {
 
@@ -14,22 +16,24 @@ export default class extends Event {
 
 	async run(guild) {
 		if (!guild.available) return;
-		if (!Links.GuildWebhook) return;
+		await prisma.guild.delete({ where: { guildId: guild.id } });
 
-		const webhook = new WebhookClient(parseWebhookURL(Links.GuildWebhook));
-		const guildOwner = await guild.fetchOwner();
+		if (Links.GuildWebhook) {
+			const webhook = new WebhookClient(parseWebhookURL(Links.GuildWebhook));
+			const guildOwner = await guild.fetchOwner();
 
-		const embed = new EmbedBuilder()
-			.setColor(Colors.Red)
-			.setTitle(`${this.client.user.username} left a Server!`)
-			.setThumbnail(guild.iconURL({ size: 512 }))
-			.setDescription([
-				`***Server:*** ${guild.name} (\`${guild.id}\`)`,
-				`***Owner:*** ${guildOwner.user.tag} (\`${guildOwner.id}\`)`
-			].join('\n'))
-			.setFooter({ text: `${this.client.guilds.cache.size.formatNumber()} guilds | ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).formatNumber()} users`, iconURL: this.client.user.avatarURL() });
+			const embed = new EmbedBuilder()
+				.setColor(Colors.Red)
+				.setTitle(`${this.client.user.username} left a Server!`)
+				.setThumbnail(guild.iconURL({ size: 512 }))
+				.setDescription([
+					`***Server:*** ${guild.name} (\`${guild.id}\`)`,
+					`***Owner:*** ${guildOwner.user.tag} (\`${guildOwner.id}\`)`
+				].join('\n'))
+				.setFooter({ text: `${this.client.guilds.cache.size.formatNumber()} guilds | ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).formatNumber()} users`, iconURL: this.client.user.avatarURL() });
 
-		return webhook.send({ embeds: [embed], avatarURL: this.client.user.displayAvatarURL({ size: 4096 }), username: this.client.user.username });
+			return webhook.send({ embeds: [embed], avatarURL: this.client.user.displayAvatarURL({ size: 4096 }), username: this.client.user.username });
+		}
 	}
 
 }
