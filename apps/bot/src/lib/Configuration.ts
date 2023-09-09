c74bac6771fd4db269ac63111bf59662b573f338
Rygent <46462403+Rygent@users.noreply.github.com>
2023-09-09 07:10:30 +0700

    refactor: better import name

--- apps/bot/src/lib/Configuration.ts
+++ apps/bot/src/lib/Configuration.ts
@@ -1,9 +1,9 @@
 import type { PermissionsString } from 'discord.js';
 import { Env } from '@aviana/env';
-import pkg from '../../package.json' assert { type: 'json' };
+import packageJson from '../../package.json' assert { type: 'json' };
 
 export const token = Env.DiscordToken;
-export const version = (Env.ClientVersion ??= pkg.version);
+export const version = (Env.ClientVersion ??= packageJson.version);
 export const prefix = Env.ClientPrefix;
 export const owners = Env.ClientOwners;
 export const debug = Env.DebugMode;
