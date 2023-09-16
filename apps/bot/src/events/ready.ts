cc2eff899444b6f3a2761e099b427bc5a251854c
Rygent <46462403+Rygent@users.noreply.github.com>
2023-09-16 12:46:13 +0700

    refactor: remove replacement string

--- apps/bot/src/events/ready.ts
+++ apps/bot/src/events/ready.ts
@@ -15,9 +15,6 @@ export default class extends Event {
 	}
 
 	public async run() {
-		const guildCount = formatNumber(this.client.guilds.cache.size);
-		const userCount = formatNumber(this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0));
-
 		this.client.logger.log(`Logged in as ${redBright(underline(`${this.client.user.tag}`))}`);
 		this.client.logger.log(
 			`Loaded ${formatNumber(this.client.commands.size + this.client.interactions.size)} commands & ${formatNumber(
@@ -26,11 +23,7 @@ export default class extends Event {
 		);
 
 		if (Env.CustomStatus) {
-			const status = Env.CustomStatus.replace('{version}', this.client.version)
-				.replace('{guildCount}', guildCount)
-				.replace('{userCount}', userCount);
-
-			this.client.user.setActivity({ name: status, type: ActivityType.Custom });
+			this.client.user.setActivity({ name: Env.CustomStatus, type: ActivityType.Custom });
 		}
 
 		const guilds = await this.client.guilds.fetch();
