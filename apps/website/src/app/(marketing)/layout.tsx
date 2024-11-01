eee2fadb2ae8cdb562204859e2f6a9b8c8837c46
Rygent <46462403+rygent@users.noreply.github.com>
2024-11-01 12:54:37 +0700

    refactor: change header opacity

--- apps/website/src/app/(marketing)/layout.tsx
+++ apps/website/src/app/(marketing)/layout.tsx
@@ -7,7 +7,7 @@ export default function MarketingLayout({ children }: React.PropsWithChildren) {
 	return (
 		<>
 			<Banner text={siteConfig.header.banner.text} dismissable />
-			<SiteHeader transparentMode="top" />
+			<SiteHeader />
 			<>{children}</>
 			<SiteFooter />
 		</>
--- apps/website/src/components/site-header.tsx
+++ apps/website/src/components/site-header.tsx
@@ -1,61 +1,43 @@
-'use client';
-
-import * as React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { DesktopNav } from '@/components/desktop-nav';
 import { siteConfig } from '@/config';
-import { buttonVariants, Github } from '@elvia/ui';
-import { cn } from '@elvia/utils';
+import { Button, Github } from '@elvia/ui';
 
-interface HeaderProps extends React.PropsWithChildren {
-	/**
-	 * When to use transparent header
-	 * @defaultValue none
-	 */
-	transparentMode?: 'always' | 'top' | 'none';
-}
-
-export function SiteHeader({ transparentMode = 'none' }: HeaderProps) {
-	const [transparent, setTransparent] = React.useState(transparentMode !== 'none');
-
-	React.useEffect(() => {
-		if (transparentMode !== 'top') return;
-
-		const listener = (): void => {
-			setTransparent(window.scrollY < 10);
-		};
-
-		listener();
-		window.addEventListener('scroll', listener);
-		return () => {
-			window.removeEventListener('scroll', listener);
-		};
-	}, [transparentMode]);
-
+export function SiteHeader() {
 	return (
-		<header
-			className={cn('sticky top-0 z-50 w-full', {
-				'border-transparent': transparent,
-				'border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60': !transparent
-			})}
-		>
-			<div className="container flex h-14 max-w-screen-2xl items-center">
-				<DesktopNav />
-				<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-					<nav className="flex items-center">
-						<Link href={siteConfig.external.links.github} target="_blank" rel="noreferrer">
-							<div
-								className={cn(
-									buttonVariants({
-										variant: 'ghost'
-									}),
-									'h-8 w-8 px-0'
-								)}
-							>
-								<Github className="h-4 w-4" />
+		<header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/90">
+			<div className="container flex h-14 max-w-screen-2xl items-center justify-between">
+				<div className="flex items-center">
+					<Link
+						href="/"
+						className="mr-4 flex items-center space-x-2 transition-opacity duration-150 hover:opacity-80 lg:mr-6"
+					>
+						<Image
+							alt={siteConfig.global.name}
+							src={siteConfig.global.logo}
+							width={24}
+							height={24}
+							className="rounded-md"
+							aria-label={siteConfig.global.name}
+						/>
+						<span className="inline-block font-bold">{siteConfig.global.name}</span>
+					</Link>
+					<DesktopNav />
+				</div>
+				<div className="flex items-center">
+					<nav className="flex items-center space-x-2 md:space-x-4">
+						<Button
+							variant="ghost"
+							size="icon"
+							className="h-8 w-8 focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"
+							asChild
+						>
+							<Link href={siteConfig.external.links.github} target="_blank" rel="noreferrer">
+								<Github size={24} />
 								<span className="sr-only">GitHub</span>
-							</div>
-						</Link>
+							</Link>
+						</Button>
 					</nav>
 				</div>
 			</div>
