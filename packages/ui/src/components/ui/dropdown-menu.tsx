a621997b958f10c8dceaf9a68446f5e3e0d9b819
Rygent <46462403+rygent@users.noreply.github.com>
2024-11-06 06:49:16 +0700

    refactor: change icon to lucide

--- packages/ui/src/components/ui/dropdown-menu.tsx
+++ packages/ui/src/components/ui/dropdown-menu.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
-import { CheckIcon, ChevronRightIcon, DotFilledIcon } from '@radix-ui/react-icons';
+import { Check, ChevronRight, Circle } from 'lucide-react';
 
 import { cn } from '@elvia/utils';
 
@@ -34,7 +34,7 @@ const DropdownMenuSubTrigger = React.forwardRef<
 		{...props}
 	>
 		{children}
-		<ChevronRightIcon className="ml-auto" />
+		<ChevronRight className="ml-auto" />
 	</DropdownMenuPrimitive.SubTrigger>
 ));
 DropdownMenuSubTrigger.displayName = DropdownMenuPrimitive.SubTrigger.displayName;
@@ -106,7 +106,7 @@ const DropdownMenuCheckboxItem = React.forwardRef<
 	>
 		<span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
 			<DropdownMenuPrimitive.ItemIndicator>
-				<CheckIcon className="h-4 w-4" />
+				<Check className="h-4 w-4" />
 			</DropdownMenuPrimitive.ItemIndicator>
 		</span>
 		{children}
@@ -128,7 +128,7 @@ const DropdownMenuRadioItem = React.forwardRef<
 	>
 		<span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
 			<DropdownMenuPrimitive.ItemIndicator>
-				<DotFilledIcon className="h-4 w-4 fill-current" />
+				<Circle className="h-2 w-2 fill-current" />
 			</DropdownMenuPrimitive.ItemIndicator>
 		</span>
 		{children}
--- packages/ui/src/components/ui/select.tsx
+++ packages/ui/src/components/ui/select.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import * as React from 'react';
-import { CaretSortIcon, CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
 import * as SelectPrimitive from '@radix-ui/react-select';
+import { Check, ChevronDown, ChevronUp } from 'lucide-react';
 
 import { cn } from '@elvia/utils';
 
@@ -26,7 +26,7 @@ const SelectTrigger = React.forwardRef<
 	>
 		{children}
 		<SelectPrimitive.Icon asChild>
-			<CaretSortIcon className="h-4 w-4 opacity-50" />
+			<ChevronDown className="h-4 w-4 opacity-50" />
 		</SelectPrimitive.Icon>
 	</SelectPrimitive.Trigger>
 ));
@@ -41,7 +41,7 @@ const SelectScrollUpButton = React.forwardRef<
 		className={cn('flex cursor-default items-center justify-center py-1', className)}
 		{...props}
 	>
-		<ChevronUpIcon />
+		<ChevronUp className="h-4 w-4" />
 	</SelectPrimitive.ScrollUpButton>
 ));
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
@@ -55,7 +55,7 @@ const SelectScrollDownButton = React.forwardRef<
 		className={cn('flex cursor-default items-center justify-center py-1', className)}
 		{...props}
 	>
-		<ChevronDownIcon />
+		<ChevronDown className="h-4 w-4" />
 	</SelectPrimitive.ScrollDownButton>
 ));
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
@@ -114,7 +114,7 @@ const SelectItem = React.forwardRef<
 	>
 		<span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
 			<SelectPrimitive.ItemIndicator>
-				<CheckIcon className="h-4 w-4" />
+				<Check className="h-4 w-4" />
 			</SelectPrimitive.ItemIndicator>
 		</span>
 		<SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
