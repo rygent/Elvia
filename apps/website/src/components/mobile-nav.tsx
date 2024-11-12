8b2bab2ab1648a9e7b153bcdc3f160dc4ac4671d
Rygent <46462403+rygent@users.noreply.github.com>
2024-11-13 05:27:18 +0700

    refactor: disable zoom animation

--- apps/website/src/components/mobile-nav.tsx
+++ apps/website/src/components/mobile-nav.tsx
@@ -42,7 +42,7 @@ export function MobileNav() {
 					<span className="sr-only">Menu</span>
 				</Button>
 			</PopoverTrigger>
-			<PopoverContent className="z-40 h-[calc(100vh-4.0625rem)] w-screen animate-none rounded-none border-none bg-background/90 p-0 px-3 shadow-none backdrop-blur transition-transform supports-[backdrop-filter]:bg-background/90 md:hidden">
+			<PopoverContent className="z-40 h-[calc(100vh-4.0625rem)] w-screen animate-none rounded-none border-none bg-background/90 p-0 px-3 shadow-none backdrop-blur transition-transform data-[state=closed]:!zoom-out-100 data-[state=open]:!zoom-in-100 supports-[backdrop-filter]:bg-background/90 md:hidden">
 				<ScrollArea>
 					<section className="my-3 px-3">
 						<PopoverClose asChild>
