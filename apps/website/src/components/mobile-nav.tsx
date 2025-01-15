7a8dd4f7a1da701665d2950ee5d58f64ef164963
Rygent <46462403+rygent@users.noreply.github.com>
2025-01-15 21:50:00 +0700

    refactor: change menu icon

--- apps/website/src/components/mobile-nav.tsx
+++ apps/website/src/components/mobile-nav.tsx
@@ -15,34 +15,41 @@ import {
 	PopoverTrigger,
 	ScrollArea
 } from '@elvia/ui';
-import { ExternalLink, Hamburger } from '@elvia/ui/icons';
+import { ExternalLink } from '@elvia/ui/icons';
 import { cn } from '@elvia/utils';
-import '@/styles/hamburger.css';
 
 export function MobileNav({ className }: React.ComponentProps<'button'>) {
-	const [isOpen, setIsOpen] = React.useState<boolean>(false);
+	const [open, setOpen] = React.useState<boolean>(false);
 
 	React.useEffect(() => {
-		if (isOpen) {
+		if (open) {
 			document.body.classList.add('overflow-hidden');
 		} else {
 			document.body.classList.remove('overflow-hidden');
 		}
-	}, [isOpen]);
+	}, [open]);
 
 	return (
-		<Popover open={isOpen} onOpenChange={setIsOpen}>
+		<Popover open={open} onOpenChange={setOpen}>
 			<PopoverTrigger asChild>
 				<Button
 					variant="ghost"
 					size="icon"
 					className={cn(
-						'hamburger h-8 w-8 rounded-full border hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden [&_svg]:size-6',
+						'group relative flex h-8 w-8 flex-col gap-0 rounded-full border pe-[6px] ps-[6px] duration-200 hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden',
 						className
 					)}
 				>
-					<Hamburger className={cn('h-6 w-6', { open: isOpen })} />
-					<span className="sr-only">Menu</span>
+					<div
+						data-position="top"
+						className="absolute h-[1.5px] w-[14px] translate-y-[-3.5px] bg-foreground transition-transform group-data-[state=open]:translate-y-0 group-data-[state=open]:rotate-45 group-data-[state=open]:scale-110"
+						style={{ transitionDuration: '160ms', transitionTimingFunction: 'cubic-bezier(0.31,0.05,0.43,1.02)' }}
+					></div>
+					<div
+						data-position="bottom"
+						className="absolute h-[1.5px] w-[14px] translate-y-[3.5px] bg-foreground transition-transform group-data-[state=open]:translate-y-0 group-data-[state=open]:-rotate-45 group-data-[state=open]:scale-110"
+						style={{ transitionDuration: '160ms', transitionTimingFunction: 'cubic-bezier(0.31,0.05,0.43,1.02)' }}
+					></div>
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className="z-40 h-[calc(100vh-4.0625rem)] w-screen animate-none rounded-none border-none bg-background/90 p-0 px-3 shadow-none backdrop-blur transition-transform data-[state=closed]:!zoom-out-100 data-[state=open]:!zoom-in-100 supports-[backdrop-filter]:bg-background/90 lg:hidden">
--- packages/ui/src/icons/index.tsx
+++ packages/ui/src/icons/index.tsx
@@ -1,5 +1,3 @@
-export * from './hamburger';
-
 export {
 	ArrowRight,
 	ChevronDown,
