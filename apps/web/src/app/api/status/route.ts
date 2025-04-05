4e18ce4aa1036051dc92aba86cbd7f8e41dcaa37
Rygent <46462403+rygent@users.noreply.github.com>
2025-04-06 04:58:29 +0700

    refactor: replace fetch with fetcher in status route

--- apps/web/src/app/api/status/route.ts
+++ apps/web/src/app/api/status/route.ts
@@ -1,16 +1,17 @@
 import { NextResponse } from 'next/server';
+import { fetcher } from '@/lib/fetcher';
 import type { UptimeMonitorResponse } from '@/types/betterstack';
 import { env } from '@/env';
 
 export async function GET() {
-	const response: UptimeMonitorResponse = await fetch(
+	const response: UptimeMonitorResponse = await fetcher(
 		'https://uptime.betterstack.com/api/v2/monitor-groups/1384070/monitors',
 		{
 			headers: {
 				Authorization: `Bearer ${env.BETTERSTACK_API_KEY}`
 			}
 		}
-	).then((res) => res.json());
+	);
 
 	const data = response.data.map((monitor) => ({
 		name: monitor.attributes.pronounceable_name,
