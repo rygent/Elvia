a125cede8695bef773bd1f41feeeb29ea31c6a1a
Rygent <46462403+rygent@users.noreply.github.com>
2025-04-22 07:36:41 +0700

    refactor(web): rewrite header and mobile menu

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,41 +1,124 @@
 import * as React from 'react';
 import Link from 'next/link';
 import { Logo, Topgg } from '@/components/icons';
-import { MainNav } from '@/components/main-nav';
-import { MobileNav } from '@/components/mobile-nav';
+import { MobileMenu } from '@/components/mobile-menu';
 import { siteConfig } from '@/config';
-import { Button } from '@elvia/ui';
+import {
+	Button,
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
+	NavigationMenuViewport
+} from '@elvia/ui';
+import { cn } from '@elvia/utils';
+import { ExternalLink } from 'lucide-react';
 
-export function Header() {
+export function Header({ className, ...props }: React.ComponentProps<'header'>) {
 	return (
-		<header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/90 sticky top-0 z-50 w-full px-6 backdrop-blur max-md:px-4">
-			<div className="mx-auto flex h-14 max-w-7xl items-center justify-between">
-				<div className="flex items-center">
-					<Link href="/" className="flex items-center justify-center gap-x-2 lg:mr-5">
-						<Logo className="h-5 w-5" />
-						<span className="font-cal inline-block text-xl leading-5 font-bold tracking-wide whitespace-nowrap">
-							{siteConfig.global.name}
-						</span>
-					</Link>
-					<MainNav />
+		<header
+			className={cn(
+				'bg-background/95 supports-[backdrop-filter]:bg-background/90 sticky top-0 z-50 w-full px-6 backdrop-blur max-md:px-4',
+				className
+			)}
+			{...props}
+		>
+			<nav className="mx-auto flex h-14 w-full max-w-7xl flex-row items-center">
+				<Link href="/" className="flex items-center justify-center gap-x-2 lg:mr-5">
+					<Logo className="h-5 w-5" />
+					<span className="font-cal inline-block text-xl leading-5 font-bold tracking-wide whitespace-nowrap">
+						{siteConfig.global.name}
+					</span>
+				</Link>
+				<NavigationBar className="max-md:hidden" />
+				<div className="flex flex-1 flex-row items-center justify-end gap-1.5">
+					<Button
+						variant="ghost"
+						size="icon"
+						className="h-8 w-8 rounded-full border focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 [&_svg]:size-4"
+						asChild
+					>
+						<Link href={siteConfig.external.links.topgg} target="_blank" rel="noreferrer">
+							<Topgg className="h-4 w-4" />
+							<span className="sr-only">Top.gg</span>
+						</Link>
+					</Button>
+					<MobileMenu />
 				</div>
-				<div className="flex items-center gap-2">
-					<nav className="flex items-center gap-0.5">
-						<Button
-							variant="ghost"
-							size="icon"
-							className="h-8 w-8 rounded-full border focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 [&_svg]:size-4"
-							asChild
-						>
-							<Link href={siteConfig.external.links.topgg} target="_blank" rel="noreferrer">
-								<Topgg className="h-4 w-4" />
-								<span className="sr-only">Top.gg</span>
-							</Link>
-						</Button>
-					</nav>
-					<MobileNav />
-				</div>
-			</div>
+			</nav>
 		</header>
+	);
+}
+
+function NavigationBar({ ...props }: React.ComponentProps<typeof NavigationMenu>) {
+	return (
+		<NavigationMenu viewport={false} data-viewport {...props}>
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<NavigationMenuTrigger className="text-muted-foreground hover:bg-accent/50 focus:bg-accent/50 bg-transparent">
+						Resources
+					</NavigationMenuTrigger>
+					<NavigationMenuContent className="p-0">
+						<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+							<li className="row-span-3">
+								<NavigationMenuLink
+									className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none select-none hover:bg-transparent focus:shadow-md"
+									asChild
+								>
+									<Link href="/invite">
+										<Logo className="text-foreground size-8" />
+										<div className="font-cal mt-4 mb-2 text-lg font-medium">{siteConfig.global.name}</div>
+										<p className="text-muted-foreground text-sm leading-tight">{siteConfig.global.description}</p>
+									</Link>
+								</NavigationMenuLink>
+							</li>
+							<ListItem href="/" title="Commands">
+								A list of available commands.
+							</ListItem>
+							<ListItem href="/" title="Blog">
+								The latest posts and changes.
+							</ListItem>
+							<ListItem href="/" title="Changelog">
+								See what changes.
+							</ListItem>
+						</ul>
+					</NavigationMenuContent>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<NavigationMenuLink
+						className={cn(
+							navigationMenuTriggerStyle(),
+							'text-muted-foreground hover:bg-accent/50 focus:bg-accent/50 flex-row bg-transparent'
+						)}
+						asChild
+					>
+						<Link href={siteConfig.external.links.topgg} target="_blank" rel="noreferrer">
+							Vote on Top.gg
+							<ExternalLink strokeWidth={1.5} className="size-3.5 text-current" />
+						</Link>
+					</NavigationMenuLink>
+				</NavigationMenuItem>
+			</NavigationMenuList>
+			<NavigationMenuViewport className="rounded-xl" />
+		</NavigationMenu>
+	);
+}
+
+function ListItem({ title, children, href, ...props }: React.ComponentPropsWithoutRef<'li'> & { href: string }) {
+	return (
+		<li {...props}>
+			<NavigationMenuLink
+				className="hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block space-y-1 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none"
+				asChild
+			>
+				<Link href={href}>
+					<div className="text-sm leading-none font-medium">{title}</div>
+					<p className="text-muted-foreground line-clamp-2 text-sm leading-snug">{children}</p>
+				</Link>
+			</NavigationMenuLink>
+		</li>
 	);
 }
---
+++ apps/web/src/components/mobile-menu.tsx
@@ -0,0 +1,150 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+import { siteConfig } from '@/config';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, Button, ScrollArea } from '@elvia/ui';
+import { cn } from '@elvia/utils';
+import { Portal } from '@radix-ui/react-portal';
+import { ExternalLink } from 'lucide-react';
+
+export function MobileMenu({ className, ...props }: React.ComponentProps<typeof Button>) {
+	const [open, setOpen] = React.useState<boolean>(false);
+	const toggleMenu = () => setOpen(!open);
+	const closeMenu = () => setOpen(false);
+
+	React.useEffect(() => {
+		const mediaQueryList = window.matchMedia('(min-width: 1024px)');
+
+		const handleChange = () => {
+			setOpen((isOpen) => (isOpen ? !mediaQueryList.matches : false));
+		};
+
+		handleChange();
+		mediaQueryList.addEventListener('change', handleChange);
+		return () => mediaQueryList.removeEventListener('change', handleChange);
+	}, []);
+
+	React.useEffect(() => {
+		if (open) {
+			document.body.classList.add('overflow-hidden');
+		} else {
+			document.body.classList.remove('overflow-hidden');
+		}
+	}, [open]);
+
+	return (
+		<>
+			<Button
+				variant="ghost"
+				size="icon"
+				data-state={getState(open)}
+				className={cn(
+					'group relative flex h-8 w-8 flex-col gap-0 rounded-full border ps-[6px] pe-[6px] duration-200 hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0',
+					className
+				)}
+				onClick={toggleMenu}
+				{...props}
+			>
+				<div
+					data-position="top"
+					className="bg-foreground absolute h-[1.5px] w-[14px] translate-y-[-3.5px] transition-transform group-data-[state=open]:translate-y-0 group-data-[state=open]:scale-110 group-data-[state=open]:rotate-45"
+					style={{ transitionDuration: '160ms', transitionTimingFunction: 'cubic-bezier(0.31,0.05,0.43,1.02)' }}
+				></div>
+				<div
+					data-position="bottom"
+					className="bg-foreground absolute h-[1.5px] w-[14px] translate-y-[3.5px] transition-transform group-data-[state=open]:translate-y-0 group-data-[state=open]:scale-110 group-data-[state=open]:-rotate-45"
+					style={{ transitionDuration: '160ms', transitionTimingFunction: 'cubic-bezier(0.31,0.05,0.43,1.02)' }}
+				></div>
+			</Button>
+			{open && (
+				<Portal
+					className={cn(
+						'bg-background/95 supports-[backdrop-filter]:bg-background/90 fixed top-14 right-0 z-50 flex h-dvh w-screen flex-col overflow-visible px-3 backdrop-blur transition-transform'
+					)}
+				>
+					<ScrollArea>
+						<section className="my-3 px-3">
+							<Button variant="default" className="h-10 w-full text-base shadow-none" onClick={closeMenu}>
+								Support
+							</Button>
+						</section>
+						<section className="py-3">
+							<Accordion type="multiple" className="px-3">
+								<AccordionItem value="resources" className="border-none">
+									<AccordionTrigger className="text-muted-foreground h-10 py-2 text-base hover:no-underline">
+										Resources
+									</AccordionTrigger>
+									<AccordionContent className="pb-0">
+										<Button
+											variant="link"
+											className="text-muted-foreground hover:text-foreground h-10 w-full justify-start px-0 text-base hover:no-underline"
+											onClick={closeMenu}
+											asChild
+										>
+											<Link href="/">Commands</Link>
+										</Button>
+										<Button
+											variant="link"
+											className="text-muted-foreground hover:text-foreground h-10 w-full justify-start px-0 text-base hover:no-underline"
+											onClick={closeMenu}
+											asChild
+										>
+											<Link href="/">Blog</Link>
+										</Button>
+										<Button
+											variant="link"
+											className="text-muted-foreground hover:text-foreground h-10 w-full justify-start px-0 text-base hover:no-underline"
+											onClick={closeMenu}
+											asChild
+										>
+											<Link href="/">Changelog</Link>
+										</Button>
+									</AccordionContent>
+								</AccordionItem>
+								<AccordionItem value="legal" className="border-none">
+									<AccordionTrigger className="text-muted-foreground h-10 py-2 text-base hover:no-underline">
+										Legal
+									</AccordionTrigger>
+									<AccordionContent className="pb-0">
+										<Button
+											variant="link"
+											className="text-muted-foreground hover:text-foreground h-10 w-full justify-start px-0 text-base hover:no-underline"
+											onClick={closeMenu}
+											asChild
+										>
+											<Link href="/legal/terms">Terms of Service</Link>
+										</Button>
+										<Button
+											variant="link"
+											className="text-muted-foreground hover:text-foreground h-10 w-full justify-start px-0 text-base hover:no-underline"
+											onClick={closeMenu}
+											asChild
+										>
+											<Link href="/legal/privacy">Privacy Policy</Link>
+										</Button>
+									</AccordionContent>
+								</AccordionItem>
+							</Accordion>
+							<Button
+								variant="link"
+								className="text-muted-foreground hover:text-foreground h-10 w-full justify-start px-3 text-base hover:no-underline"
+								onClick={closeMenu}
+								asChild
+							>
+								<Link href={siteConfig.external.links.topgg} className="gap-1" target="_blank" rel="noreferrer">
+									Vote on Top.gg
+									<ExternalLink size={14} strokeWidth={1.5} />
+								</Link>
+							</Button>
+						</section>
+					</ScrollArea>
+				</Portal>
+			)}
+		</>
+	);
+}
+
+function getState(open: boolean) {
+	return open ? 'open' : 'closed';
+}
