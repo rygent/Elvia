5d99d6a7b51b18ea0c0dfef3337c11d3b38378fa
Rygent <46462403+rygent@users.noreply.github.com>
2025-06-28 07:48:44 +0700

    refactor: implementation core lib

--- apps/bot/src/commands/slash/tag.ts
+++ apps/bot/src/commands/slash/tag.ts
@@ -1,5 +1,4 @@
-import { Client } from '@/lib/structures/client.js';
-import { Command } from '@/lib/structures/command.js';
+import { CoreClient, CoreCommand } from '@elvia/core';
 import {
 	ApplicationCommandOptionType,
 	ApplicationCommandType,
@@ -11,8 +10,8 @@ import type { AutocompleteInteraction, ChatInputCommandInteraction } from 'disco
 import { inlineCode } from '@discordjs/formatters';
 import { prisma } from '@elvia/database';
 
-export default class extends Command {
-	public constructor(client: Client<true>) {
+export default class extends CoreCommand {
+	public constructor(client: CoreClient<true>) {
 		super(client, {
 			type: ApplicationCommandType.ChatInput,
 			name: 'tag',
