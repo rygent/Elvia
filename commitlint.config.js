c71db024a24bde00ac531656c2313bc656f262d4
Rygent <46462403+rygent@users.noreply.github.com>
2025-08-01 08:33:20 +0700

    chore: allow `!` to indicate breaking changes

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,8 @@ const config = {
 			'always',
 			['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'types']
 		],
-		'scope-case': [0]
+		'scope-case': [0],
+		'subject-exclamation-mark': [0]
 	}
 };
 
