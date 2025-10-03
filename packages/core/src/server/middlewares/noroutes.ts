839fae517913210f4755d5a63ea74b6aa9f5d775
rygent <46462403+rygent@users.noreply.github.com>
2025-10-03 09:13:58 +0700

    refactor(webserver)!: use `fastify` instead (#1299)

--- packages/core/src/server/middlewares/noroutes.ts
+++
@@ -1,6 +0,0 @@
-import { type NextFunction, type Request, type Response } from 'express';
-import createError from 'http-errors';
-
-export default (_: Request, __: Response, next: NextFunction) => {
-	next(createError(404));
-};
